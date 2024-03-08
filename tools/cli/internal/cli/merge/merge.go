61a43689de092332b7118c15723495a93c41abef
Andrea Angiolillo <andrea.angiolillo@mongodb.com>
2024-03-08 15:44:43 +0000

    CLOUDP-236398: Adding merge command skeleton (#6)

--- tools/cli/internal/cli/merge/merge.go
+++ tools/cli/internal/cli/merge/merge.go
@@ -15,31 +15,60 @@
 package merge
 
 import (
+	"fmt"
+	"log"
+	"mongodb/openapi/tools/cli/internal/cli/flag"
+	"mongodb/openapi/tools/cli/internal/cli/usage"
+	"os"
+
 	"github.com/spf13/cobra"
-	"github.com/tufin/oasdiff/load"
+)
+
+const (
+	DefaultOutputFileName = "FOAS.json"
 )
 
 type Opts struct {
-	Base *load.SpecInfo
+	basePath      string
+	outputPath    string
+	externalPaths []string
 }
 
 func (o *Opts) Run(_ []string) error {
 	// To add in follow up PR: CLOUDP-225849
-	return nil
+	return o.saveFile([]byte("test"))
 }
 
 func (o *Opts) PreRunE(_ []string) error {
-	// To Add in follow up PR: CLOUDP-225849
+	if o.basePath == "" {
+		return fmt.Errorf("no base OAS detected. Please, use the flag %s to include the base OAS", flag.Base)
+	}
+
+	if o.externalPaths == nil {
+		return fmt.Errorf("no external OAS detected. Please, use the flag %s to include at least one OAS", flag.External)
+	}
+
 	return nil
 }
 
+func (o *Opts) saveFile(data []byte) error {
+	if err := os.WriteFile(o.outputPath, data, 0o600); err != nil {
+		return err
+	}
+
+	log.Printf("\nMerged spec was saved in '%s'.\n\n", o.outputPath)
+	return nil
+}
+
+// Builder builds the merge command with the following signature:
+// merge -b base-oas -e external-oas-1 -e external-oas-2
 func Builder() *cobra.Command {
 	opts := &Opts{}
 
 	cmd := &cobra.Command{
-		Use:   "merge [base-spec] [spec-1] [spec-2] [spec-3] ... [spec-n]",
+		Use:   "merge -b base-spec [-e spec]...",
 		Short: "Merge Open API specifications into a base spec.",
-		Args:  cobra.MinimumNArgs(2),
+		Args:  cobra.NoArgs,
 		PreRunE: func(_ *cobra.Command, args []string) error {
 			return opts.PreRunE(args)
 		},
@@ -48,5 +77,8 @@ func Builder() *cobra.Command {
 		},
 	}
 
+	cmd.Flags().StringVarP(&opts.basePath, flag.Base, flag.BaseShort, "", usage.Base)
+	cmd.Flags().StringArrayVarP(&opts.externalPaths, flag.External, flag.ExternalShort, nil, usage.External)
+	cmd.Flags().StringVarP(&opts.outputPath, flag.Output, flag.OutputShort, DefaultOutputFileName, usage.Output)
 	return cmd
 }
---
+++ tools/cli/internal/cli/validator/command.go
@@ -0,0 +1,41 @@
+// Copyright 2024 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package validator
+
+import (
+	"testing"
+
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+)
+
+// ValidateSubCommandsAndFlags validates a cobra.Command, verifying the number of sub commands
+// and the flags that are being defined for it.
+func ValidateSubCommandsAndFlags(t *testing.T, subject *cobra.Command, nSubCommands int, flags []string) {
+	t.Helper()
+	a := assert.New(t)
+	if len(subject.Commands()) != nSubCommands {
+		t.Errorf("Sub command count mismatch. Expected %d, got %d. "+
+			"Check the CmdValidator for your command.\n", nSubCommands, len(subject.Commands()))
+	}
+	if len(flags) == 0 {
+		a.False(subject.HasAvailableFlags(), "expected command to not have flags but it does")
+		return
+	}
+	a.True(subject.HasAvailableFlags(), "expected command to have flag but has none")
+	for _, f := range flags {
+		a.NotNilf(subject.Flags().Lookup(f), "command has no flag: %s", f)
+	}
+}
