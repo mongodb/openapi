e98196ec5eec542b3e8e3962a314e75197ceca87
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-03-13 17:12:57 +0000

    CLOUDP-225849: add merging logic

--- tools/cli/internal/openapi/oasdiff.go
+++ tools/cli/internal/openapi/oasdiff.go
@@ -15,9 +15,11 @@
 package openapi
 
 import (
+	"fmt"
 	"github.com/mongodb/openapi/tools/cli/internal/openapi/errors"
 	"github.com/tufin/oasdiff/diff"
 	"github.com/tufin/oasdiff/load"
+	"os"
 )
 
 type OasDiff struct {
@@ -29,7 +31,19 @@ type OasDiff struct {
 }
 
 func (o OasDiff) mergeSpecIntoBase() error {
-	return o.mergePaths()
+	if err := o.mergePaths(); err != nil {
+		return err
+	}
+
+	if err := o.mergeTags(); err != nil {
+		return err
+	}
+
+	if err := o.mergeComponents(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (o OasDiff) mergePaths() error {
@@ -47,4 +61,141 @@ func (o OasDiff) mergePaths() error {
 
 	o.base.Spec.Paths = basePaths
 	return nil
+}
+
+func (o OasDiff) mergeTags() error {
+	tagsToMerge := o.external.Spec.Tags
+	baseTags := o.base.Spec.Tags
+	tagsSet := make(map[string]bool)
+
+	for _, v := range baseTags {
+		tagsSet[v.Name] = true
+	}
+
+	for _, v := range tagsToMerge {
+		if _, ok := tagsSet[v.Name]; !ok {
+			baseTags = append(baseTags, v)
+		} else {
+			return errors.TagConflictError{
+				Entry:       v.Name,
+				Description: v.Description,
+			}
+		}
+	}
+
+	o.base.Spec.Tags = baseTags
+	return nil
+}
+
+func (o OasDiff) mergeComponents() error {
+	if err := o.mergeParameters(); err != nil {
+		return err
+	}
+
+	if err := o.mergeResponses(); err != nil {
+		return err
+	}
+
+	if err := o.mergeSchemas(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (o OasDiff) mergeParameters() error {
+	externalSpecParams := o.external.Spec.Components.Parameters
+	baseParams := o.base.Spec.Components.Parameters
+	for k, v := range externalSpecParams {
+		if _, ok := baseParams[k]; !ok {
+			baseParams[k] = v
+		} else {
+			if o.areParamsIdentical(k) {
+				// if the responses are the same, we skip
+				_, _ = fmt.Fprintf(os.Stderr, "We silently resolved the conflict with the response '%s' because the definition was identical.", k)
+				continue
+			}
+
+			// The params have the same name but different definitions
+			return errors.ParamConflictError{
+				Entry: k,
+			}
+		}
+	}
+
+	o.base.Spec.Components.Parameters = baseParams
+	return nil
+}
+
+func (o OasDiff) mergeResponses() error {
+	extResponses := o.external.Spec.Components.Responses
+	baseResponses := o.base.Spec.Components.Responses
+	for k, v := range extResponses {
+		if _, ok := baseResponses[k]; !ok {
+			baseResponses[k] = v
+		} else {
+			if o.areParamsIdentical(k) {
+				// if the params are the same, we skip
+				_, _ = fmt.Fprintf(os.Stderr, "We silently resolved the conflict with the params '%s' because the definition was identical.", k)
+				continue
+			}
+
+			// The responses have the same name but different definitions
+			return errors.ResponseConflictError{
+				Entry: k,
+			}
+		}
+	}
+
+	o.base.Spec.Components.Responses = baseResponses
+	return nil
+}
+
+func (o OasDiff) mergeSchemas() error {
+	extSchemas := o.external.Spec.Components.Schemas
+	baseSchemas := o.base.Spec.Components.Schemas
+
+	for k, schemaToMerge := range extSchemas {
+		if _, ok := baseSchemas[k]; !ok {
+			baseSchemas[k] = schemaToMerge
+		} else {
+			if o.areSchemaIdentical(k) {
+				// if the schemas are the same, we skip
+				_, _ = fmt.Fprintf(os.Stderr, "We silently resolved the conflict with the schemas '%s' because the definition was identical.", k)
+				continue
+			}
+
+			// The schemas have the same name but different definitions
+			return errors.SchemaConflictError{
+				Entry: k,
+			}
+		}
+	}
+
+	o.base.Spec.Components.Schemas = baseSchemas
+	return nil
+}
+
+func (o OasDiff) areParamsIdentical(paramName string) bool {
+	if _, ok := o.specDiff.ParametersDiff.Modified[paramName]; !ok {
+		return true
+	}
+
+	return false
+}
+
+func (o OasDiff) areResponsesIdentical(name string) bool {
+	if _, ok := o.specDiff.ResponsesDiff.Modified[name]; !ok {
+		return true
+	}
+
+	return false
+}
+
+func (o OasDiff) areSchemaIdentical(name string) bool {
+	if _, ok := o.specDiff.SchemasDiff.Modified[name]; !ok {
+		return true
+	}
+
+	return false
 }
