e9eb1ecd4d4fc6855d168c7756e87f2f9ce91419
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-05-27 16:26:58 +0100

    Update merge_test.go

--- tools/cli/internal/cli/merge/merge_test.go
+++ tools/cli/internal/cli/merge/merge_test.go
@@ -90,7 +90,7 @@ func TestOpts_PreRunE(t *testing.T) {
 		},
 		{
 			wantErr:       require.NoError,
-			basePath:      "../../../test/data/base.json",
+			basePath:      "../../../test/data/valid_base_spec.json",
 			name:          "Successful",
 			externalPaths: []string{"test"},
 		},
