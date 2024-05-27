a8b941a5ee63ef584e357b7aaeffcbe80d5f810e
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-05-27 16:15:58 +0100

    added e2e test scenarios

--- tools/cli/test/e2e/cli/cli.go
+++ tools/cli/test/e2e/cli/cli.go
@@ -18,3 +18,49 @@ func NewBin() (string, error) {
 	}
 	return cliPath, nil
 }
+
+func NewBaseSpec() (string, error) {
+	cliPath, err := filepath.Abs("../../data/valid_base_spec.json")
+	if err != nil {
+		return "", err
+	}
+	return cliPath, nil
+}
+
+func NewValidAPIRegistrySpec() (string, error) {
+	cliPath, err := filepath.Abs("../../data/valid_apiregistry_spec.json")
+	if err != nil {
+		return "", err
+	}
+	return cliPath, nil
+}
+
+func NewValidAuthNSpec() (string, error) {
+	cliPath, err := filepath.Abs("../../data/valid_authn_spec.json")
+	if err != nil {
+		return "", err
+	}
+	return cliPath, nil
+}
+func NewDuplicatedPathAPIRegistrySpec() (string, error) {
+	cliPath, err := filepath.Abs("../../data/duplicated_path_apiregistry_spec.json")
+	if err != nil {
+		return "", err
+	}
+	return cliPath, nil
+}
+func NewNotIdenticalComponentPIRegistrySpec() (string, error) {
+	cliPath, err := filepath.Abs("../../data/not_identical_component_apiregistry_spec.json")
+	if err != nil {
+		return "", err
+	}
+	return cliPath, nil
+}
+
+func NewDuplicatedTagAuthNSpec() (string, error) {
+	cliPath, err := filepath.Abs("../../data/duplicated_tag_authn_spec.json")
+	if err != nil {
+		return "", err
+	}
+	return cliPath, nil
+}
--- tools/cli/test/e2e/cli/merge_test.go
+++ tools/cli/test/e2e/cli/merge_test.go
@@ -1,10 +1,12 @@
 package cli
 
 import (
+	"bytes"
 	"os"
 	"os/exec"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -12,14 +14,102 @@ func TestMerge(t *testing.T) {
 	cliPath, err := NewBin()
 	require.NoError(t, err)
 
-	t.Run("Use --help flag", func(t *testing.T) {
+	t.Run("Merge valid specs", func(t *testing.T) {
+		base, err := NewBaseSpec()
+		require.NoError(t, err)
+		external, err := NewValidAPIRegistrySpec()
+		require.NoError(t, err)
+
 		cmd := exec.Command(cliPath,
 			"merge",
-			"--help",
+			"-b",
+			base,
+			"-e",
+			external,
+		)
+
+		var o, e bytes.Buffer
+		cmd.Stdout = &o
+		cmd.Stderr = &e
+		require.NoError(t, cmd.Run(), e.String())
+
+		assert.Contains(t, o.String(), "\"openapi\"")
+		assert.Contains(t, e.String(), "We silently resolved the conflict with the schemas \"ApiError\" because the definition was identical")
+		assert.Contains(t, o.String(), "\"ApiError\":")
+	})
+
+	t.Run("Expecting Error: Merge duplicated path with base spec", func(t *testing.T) {
+		base, err := NewBaseSpec()
+		require.NoError(t, err)
+		apiRegistrySpec, err := NewDuplicatedPathAPIRegistrySpec()
+		require.NoError(t, err)
+		authnSpec, err := NewValidAuthNSpec()
+		require.NoError(t, err)
+
+		cmd := exec.Command(cliPath,
+			"merge",
+			"-b",
+			base,
+			"-e",
+			apiRegistrySpec,
+			"-e",
+			authnSpec,
 		)
 
 		cmd.Env = os.Environ()
 		resp, err := cmd.CombinedOutput()
-		require.NoError(t, err, string(resp))
+		stringResponse := string(resp)
+		require.Error(t, err, stringResponse)
+		assert.Contains(t, stringResponse, "Error: there was a conflict with the path: \"/api/atlas/v2/groups/{groupId}/events\"")
+	})
+
+	t.Run("Expecting Error: Merge duplicated tag", func(t *testing.T) {
+		base, err := NewBaseSpec()
+		require.NoError(t, err)
+		apiRegistrySpec, err := NewValidAPIRegistrySpec()
+		require.NoError(t, err)
+		authnSpec, err := NewDuplicatedTagAuthNSpec()
+		require.NoError(t, err)
+
+		cmd := exec.Command(cliPath,
+			"merge",
+			"-b",
+			base,
+			"-e",
+			apiRegistrySpec,
+			"-e",
+			authnSpec,
+		)
+
+		cmd.Env = os.Environ()
+		resp, err := cmd.CombinedOutput()
+		stringResponse := string(resp)
+		require.Error(t, err, stringResponse)
+		assert.Contains(t, stringResponse, "Error: there was a conflict with the Tag \"Events\" with the description: \"Returns information about the MongoDB Atlas Specification.\"")
+	})
+
+	t.Run("Expecting Error: not identical component", func(t *testing.T) {
+		base, err := NewBaseSpec()
+		require.NoError(t, err)
+		apiRegistrySpec, err := NewNotIdenticalComponentPIRegistrySpec()
+		require.NoError(t, err)
+		authnSpec, err := NewValidAuthNSpec()
+		require.NoError(t, err)
+
+		cmd := exec.Command(cliPath,
+			"merge",
+			"-b",
+			base,
+			"-e",
+			apiRegistrySpec,
+			"-e",
+			authnSpec,
+		)
+
+		cmd.Env = os.Environ()
+		resp, err := cmd.CombinedOutput()
+		stringResponse := string(resp)
+		require.Error(t, err, stringResponse)
+		assert.Contains(t, stringResponse, "Error: there was a conflict on a Schema component: \"ApiError\"")
 	})
 }
