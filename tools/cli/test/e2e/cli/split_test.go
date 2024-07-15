57bf8bd533935bf8bc6121bcfdff8d0a45f7e3f5
Bianca Lisle <40155621+blva@users.noreply.github.com>
2024-07-15 19:33:07 +0100

    chore: use oasdiff instead of manual diff (#70)

--- tools/cli/test/e2e/cli/split_test.go
+++ tools/cli/test/e2e/cli/split_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/tufin/oasdiff/diff"
 )
 
 var versions = []string{"2023-01-01", "2023-02-01", "2023-10-01", "2023-11-15", "2024-05-30"}
@@ -22,7 +23,7 @@ func TestSplit(t *testing.T) {
 			"-s",
 			base,
 			"-o",
-			getOutputFolder(t)+"/output.yaml",
+			getOutputFolder(t)+"/output.json",
 		)
 
 		var o, e bytes.Buffer
@@ -49,7 +50,37 @@ func getOutputFolder(t *testing.T) string {
 
 func validateFiles(t *testing.T, version string) {
 	t.Helper()
-	path, err := filepath.Abs("./output/output-" + version + ".yaml")
+	path, err := filepath.Abs("./output/output-" + version + ".json")
 	require.NoError(t, err)
 	ValidateVersionedSpec(t, NewValidAtlasSpecPath(t, version), path)
+}
+
+func ValidateVersionedSpec(t *testing.T, correctSpecPath, generatedSpecPath string) {
+	t.Helper()
+	correctSpec := newOpenAPISpec(t, correctSpecPath)
+	generatedSpec := newOpenAPISpec(t, generatedSpecPath)
+	// compare w/ oasdiff
+	d, err := diff.Get(diff.NewConfig(), correctSpec, generatedSpec)
+	require.NoError(t, err)
+
+	message := "Generated spec is not equal to the correct spec for path: " + correctSpecPath + "\n\n" +
+		"git diff --no-index " + correctSpecPath + " " + generatedSpecPath + " > diff.diff"
+
+	require.Empty(t, d.ExtensionsDiff, message)
+	require.Empty(t, d.OpenAPIDiff, message)
+	// require.Empty(t, d.InfoDiff, message) TODO: add in next PR
+	// require.Empty(t, d.EndpointsDiff) TODO: add in next PR
+	// require.Empty(t, d.PathsDiff) TODO: add in next PR
+	require.Empty(t, d.SecurityDiff, message)
+	require.Empty(t, d.ServersDiff, message)
+	require.Empty(t, d.TagsDiff, message)
+	require.Empty(t, d.ExternalDocsDiff, message)
+	require.Empty(t, d.ExamplesDiff, message)
+	// require.Empty(t, d.ComponentsDiff) TODO: add in next PR
+
+	// Components diff
+	for _, v := range d.PathsDiff.Modified {
+		require.Empty(t, v.ExtensionsDiff)
+		require.Empty(t, v.DescriptionDiff)
+	}
 }
