6222c094ae7b9c4159cbb0e2310ddec78678c2bb
Luke Sanderson <luke.sanderson@mongodb.com>
2024-07-17 15:14:50 +0100

    Add environment variables comment

--- tools/postman/scripts/convert-to-collection.sh
+++ tools/postman/scripts/convert-to-collection.sh
@@ -3,12 +3,19 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+#########################################################
+# Fetch openapi from remote file
+# Environment variables:
+#   OPENAPI_FILE_NAME - openapi file name to use
+#   OPENAPI_FOLDER - folder for saving openapi file
+#   COLLECTION_FILE_NAME - postman collection file name to save to
+#   TMP_FOLDER - folder for temporary files during transformations
+#########################################################
+
 OPENAPI_FILE_NAME=${OPENAPI_FILE_NAME:-"atlas-api.json"}
 COLLECTION_FILE_NAME=${COLLECTION_FILE_NAME:-"collection.json"}
-
 OPENAPI_FOLDER=${OPENAPI_FOLDER:-"../openapi"}
 TMP_FOLDER=${TMP_FOLDER:-"../tmp"}
-
 
 echo "Removing regex"
 jq 'del(.. | select(type == "object") | .pattern?)' "$OPENAPI_FOLDER"/"$OPENAPI_FILE_NAME" > "$TMP_FOLDER"/tmp.json
