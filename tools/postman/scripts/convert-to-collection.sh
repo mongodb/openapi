1f4677ef781b7e1aa5905139d4c39491fd33a324
Luke Sanderson <luke.sanderson@mongodb.com>
2024-07-17 14:16:10 +0100

    CLOUDP-262012: Add convert to collection script

---
+++ tools/postman/scripts/convert-to-collection.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+OPENAPI_FILE_NAME=${OPENAPI_FILE_NAME:-"atlas-api.json"}
+COLLECTION_FILE_NAME=${COLLECTION_FILE_NAME:-"collection.json"}
+
+OPENAPI_FOLDER=${OPENAPI_FOLDER:-"../openapi"}
+TMP_FOLDER=${TMP_FOLDER:-"../tmp"}
+
+
+echo "Removing regex"
+jq 'del(.. | select(type == "object") | .pattern?)' "$OPENAPI_FOLDER"/"$OPENAPI_FILE_NAME" > "$TMP_FOLDER"/tmp.json
+
+echo "Installing openapi-to-postmanv2"
+npm install
+openapi2postmanv2 -s "$TMP_FOLDER"/tmp.json -o "$TMP_FOLDER"/"$COLLECTION_FILE_NAME" -O folderStrategy=Tags
+
+rm "$TMP_FOLDER"/tmp.json
