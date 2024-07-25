22de0211f109161bf3b5252f669acec8bb28c29b
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-07-25 16:34:56 +0100

    fixes

---
+++ .github/scripts/generate_federated_spec.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+# Loop through the filtered services and construct the flag
+service_array=()
+while IFS= read -r service; do
+service_array+=("-e" "openapi-${service}.json")
+done < <(jq -r '.services[] | select(.name != "mms") | .name' foas-metadata.json)
+
+echo "Running FOAS CLI merge command with the following services: " "${service_array[@]}"
+./foascli merge -b openapi-mms.json "${service_array[@]}" -o openapi-foas.json -f json
+./foascli merge -b openapi-mms.json "${service_array[@]}" -o openapi-foas.yaml -f yaml
+
