7daf50d0d3f7bdf1873a45ddb7c86b257a72ecbe
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-08-01 14:58:03 +0100

    fix: update the foas staging release to use stage

--- .github/scripts/split_spec.sh
+++ .github/scripts/split_spec.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
+echo "Running FOAS CLI split command with the following --env=${target_env} and -o=./openapi/v2/openapi.json"
 
-env_flag=${target_env:?}
-if [[ ${env_flag} == "staging" ]]; then
-    env_flag="stage"
-fi
-
-echo "Running FOAS CLI split command with the following --env=${env_flag} and -o=./openapi/v2/openapi.json"
-
-foascli split -s openapi-foas.json --env "${env_flag}" -o ./openapi/v2/openapi.json
+foascli split -s openapi-foas.json --env "${target_env}" -o ./openapi/v2/openapi.json
 cp -rf "openapi-foas.json" "./openapi/v2.json"
 
-foascli split -s openapi-foas.yaml --env "${env_flag}" -o ./openapi/v2/openapi.yaml
+foascli split -s openapi-foas.yaml --env "${target_env}" -o ./openapi/v2/openapi.yaml
 cp -rf "openapi-foas.yaml" "./openapi/v2.yaml"
 
