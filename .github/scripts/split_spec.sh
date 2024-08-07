92868d2eb3782e6726e7dfa43de077a5ea852152
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-08-07 10:55:20 +0100

    fix: FOAS release process

--- .github/scripts/split_spec.sh
+++ .github/scripts/split_spec.sh
@@ -9,8 +9,8 @@ fi
 echo "Running FOAS CLI split command with the following --env=${env_flag} and -o=./openapi/v2/openapi.json"
 
 foascli split -s openapi-foas.json --env "${env_flag}" -o ./openapi/v2/openapi.json
-cp -rf "openapi-foas.json" "./openapi/v2.json"
+mv -f "openapi-foas.json" "./openapi/v2.json"
 
 foascli split -s openapi-foas.yaml --env "${env_flag}" -o ./openapi/v2/openapi.yaml
-cp -rf "openapi-foas.yaml" "./openapi/v2.yaml"
+mv -f "openapi-foas.yaml" "./openapi/v2.yaml"
 
