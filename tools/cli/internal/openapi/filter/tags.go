9e8ee7706acf021f37e62e93423f60586a2a92f9
Bianca <bianca.vianadeaguiar@mongodb.com>
2024-08-12 13:14:55 +0100

    Update

--- tools/cli/internal/openapi/filter/tags.go
+++ tools/cli/internal/openapi/filter/tags.go
@@ -19,7 +19,7 @@ import (
 	"github.com/getkin/kin-openapi/openapi3"
 )
 
-// TagsFilter removes tags that are not used in the operations.
+// Filter: TagsFilter removes tags that are not used in the operations.
 type TagsFilter struct {
 	oas *openapi3.T
 }
