a4048a5ae3079183ed954edab0e42a817dd745f7
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-08-21 11:00:55 +0100

    Update split_spec.sh

--- .github/scripts/split_spec.sh
+++ .github/scripts/split_spec.sh
@@ -2,12 +2,12 @@
 set -eou pipefail
 
 echo "Running FOAS CLI versions command"
-foascli versions -s openapi-foas.json -o ./openapi/v2/versions.json --env "${target_env}"
+foascli versions -s openapi-foas.json -o ./openapi/v2/versions.json --env "${target_env:?}"
 
-echo "Running FOAS CLI split command with the following --env=${target_env} and -o=./openapi/v2/openapi.json"
+echo "Running FOAS CLI split command with the following --env=${target_env:?} and -o=./openapi/v2/openapi.json"
 
-foascli split -s openapi-foas.json --env "${target_env}" -o ./openapi/v2/openapi.json
+foascli split -s openapi-foas.json --env "${target_env:?}" -o ./openapi/v2/openapi.json
 mv -f "openapi-foas.json" "./openapi/v2.json"
 
-foascli split -s openapi-foas.yaml --env "${target_env}" -o ./openapi/v2/openapi.yaml
+foascli split -s openapi-foas.yaml --env "${target_env:?}" -o ./openapi/v2/openapi.yaml
 mv -f "openapi-foas.yaml" "./openapi/v2.yaml"
