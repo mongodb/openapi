80cfa5c5cc488d5496966cfcd5807de9d49a385e
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-08-21 10:47:03 +0100

    CLOUDP-269087: Update FOAS prod release process to stop creating PR

--- .github/scripts/split_spec.sh
+++ .github/scripts/split_spec.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
-env_flag=${target_env:?}
-if [[ ${env_flag} == "staging" ]]; then
-    env_flag="stage"
-fi
+echo "Running FOAS CLI split command with the following --env=${target_env} and -o=./openapi/v2/openapi.json"
 
-echo "Running FOAS CLI split command with the following --env=${env_flag} and -o=./openapi/v2/openapi.json"
-
-foascli split -s openapi-foas.json --env "${env_flag}" -o ./openapi/v2/openapi.json
+foascli split -s openapi-foas.json --env "${target_env}" -o ./openapi/v2/openapi.json
 mv -f "openapi-foas.json" "./openapi/v2.json"
 
-foascli split -s openapi-foas.yaml --env "${env_flag}" -o ./openapi/v2/openapi.yaml
+foascli split -s openapi-foas.yaml --env "${target_env}" -o ./openapi/v2/openapi.yaml
 mv -f "openapi-foas.yaml" "./openapi/v2.yaml"
 
+echo "Running FOAS CLI versions command"
+foascli versions -s openapi-foas.json -o ./openapi/v2/versions.json --env "${target_env}"
