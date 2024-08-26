439e8de213c068f2f0cd775efa48032565be1739
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-08-26 18:00:04 +0100

    CLOUDP-269500: Add logic to create jira ticket when we file an issue

---
+++ .github/scripts/create_jira_ticket.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -eou pipefail
+
+json_response=$(curl --request POST \
+--url 'https://jira.mongodb.org/rest/api/2/issue' \
+--header 'Authorization: Bearer '"${JIRA_API_TOKEN:?}" \
+--header 'Accept: application/json' \
+--header 'Content-Type: application/json' \
+--data '{
+    "fields": {
+        "project": {
+            "id": "10984"
+        },
+        "summary": "('"${TARGET_ENV}"') The mongodb/openapi release has failed",
+        "issuetype": {
+            "id": "12"
+        },
+        "customfield_12751": [{
+                "id": "22223"
+        }],                  
+        "description": "The release process in [mongodb/openapi|https://github.com/mongodb/openapi] has failed. Please, look at the [issue-'"${ISSUE_ID}"'|https://github.com/mongodb/openapi/issues/'"${ISSUE_ID}"'] for more details.",
+        "components": [
+            {
+                "id": "35986"
+            }
+        ]
+    }
+}')
+
+echo "Response: ${json_response}"
+
+JIRA_TICKET_ID=$(echo "${json_response}" | jq -r '.key')
+
+echo "The following JIRA ticket has been created: ${JIRA_TICKET_ID}"
+echo "jira-ticket-id=${JIRA_TICKET_ID}" >> "${GITHUB_OUTPUT}"
