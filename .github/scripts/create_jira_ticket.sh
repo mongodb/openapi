09ae6adf403676674708602cb294a9da2dd6c347
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-08-26 18:21:23 +0100

    fixes

--- .github/scripts/create_jira_ticket.sh
+++ .github/scripts/create_jira_ticket.sh
@@ -11,14 +11,14 @@ json_response=$(curl --request POST \
         "project": {
             "id": "10984"
         },
-        "summary": "('"${TARGET_ENV}"') The mongodb/openapi release has failed",
+        "summary": "('"${TARGET_ENV:?}"') The '"${RELEASE_NAME:?}"' release has failed",
         "issuetype": {
             "id": "12"
         },
         "customfield_12751": [{
                 "id": "22223"
         }],                  
-        "description": "The release process in [mongodb/openapi|https://github.com/mongodb/openapi] has failed. Please, look at the [issue-'"${ISSUE_ID}"'|https://github.com/mongodb/openapi/issues/'"${ISSUE_ID}"'] for more details.",
+        "description": "The release process "'"${RELEASE_NAME:?}"'" in [mongodb/openapi|https://github.com/mongodb/openapi] has failed. Please, look at the [issue-'"${ISSUE_ID:?}"'|https://github.com/mongodb/openapi/issues/'"${ISSUE_ID}"'] for more details.",
         "components": [
             {
                 "id": "35986"
