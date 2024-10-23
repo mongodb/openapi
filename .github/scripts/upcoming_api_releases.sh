98ec78ff69f9390f52e09d59e599a3a9961c272c
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-10-23 13:48:46 +0100

    Add logic to check for a Jira ticket with the same title
    This commit update the logic in the create_jira_ticket.sh to check for a ticket with the same title before creating the ticket

--- .github/scripts/upcoming_api_releases.sh
+++ .github/scripts/upcoming_api_releases.sh
@@ -7,11 +7,11 @@ URL="https://raw.githubusercontent.com/mongodb/openapi/dev/openapi/v2/versions.j
 # Fetch the version.json file
 response=$(curl -s "${URL}")
 
-# Parse the dates from the JSON response using jq
-dates=$(echo "${response}" | jq -r '.[]')
+# Parse the version_dates from the JSON response using jq
+version_dates=$(echo "${response}" | jq -r '.[]')
 
-# Initialize an empty list to store dates within 3 weeks
-dates_within_3_weeks=()
+# Initialize an empty list to store version_dates within 3 weeks
+version_dates_within_3_weeks=()
 
 # Get the current date in seconds since epoch
 current_date=$(date +%s)
@@ -26,9 +26,9 @@ else
 fi
 
 # Iterate through each date
-for date in $dates; do
+for version_date in ${version_dates}; do
     # Convert the date to seconds since epoch with explicit format
-    date_in_seconds=$($date_command "${date}" +%s 2>/dev/null)
+    date_in_seconds=$($date_command "${version_date}" +%s 2>/dev/null)
 
     # Calculate the difference in days between the date and the current date
     diff_in_days=$(( (date_in_seconds - current_date) / (60 * 60 * 24) ))
@@ -36,13 +36,13 @@ for date in $dates; do
     # Check if the date is within 3 weeks (21 days)
     if [[ "${diff_in_days}" -ge 0 && "${diff_in_days}" -le 21 ]]; then
         # Add the date to the list if within 3 weeks
-        dates_within_3_weeks+=("${date}")
+        version_dates_within_3_weeks+=("${date}")
     fi
 done
 
-if [[ ${#dates_within_3_weeks[@]} -gt 0 ]]; then
-    echo "API Versions that will be release in the next 3 weeks: ${dates_within_3_weeks[*]}"
-    echo api_versions="${dates_within_3_weeks[*]}" >> "${GITHUB_OUTPUT:?}"
+if [[ ${#version_dates_within_3_weeks[@]} -gt 0 ]]; then
+    echo "API Versions that will be release in the next 3 weeks: ${version_dates_within_3_weeks[*]}"
+    echo api_versions="${version_dates_within_3_weeks[*]}" >> "${GITHUB_OUTPUT:?}"
 else
     echo "No API Versions that will be released within the next 3 weeks."
 fi
