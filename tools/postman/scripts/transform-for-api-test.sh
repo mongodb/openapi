eb0e85795d20dcdee0b5fb596631d89f4aad10dd
Wojciech Trocki <w.trocki@mongodb.com>
2024-12-09 11:12:01 +0100

    final tests

--- tools/postman/scripts/transform-for-api-test.sh
+++ tools/postman/scripts/transform-for-api-test.sh
@@ -18,70 +18,26 @@ if [[ ! -f "$COLLECTION_FILE_NAME" ]]; then
   exit 1
 fi
 
-
-# Test: Wrap Collection in "collection" tag
-echo "Wrapping Collection $COLLECTION_FILE_NAME in \"collection\" tag"
-jq '{"collection": .}' "$COLLECTION_FILE_NAME" > intermediateCollectionWrapped.json
-
-if jq -e '.collection' intermediateCollectionWrapped.json > /dev/null; then
-  echo "Test Passed: Collection wrapped successfully."
-else
-  echo "Test Failed: Collection wrapping failed."
-fi
-
 # Test: Disable query params by default
-echo "Disabling query params by default"
-# jq '(.. | select(.request? != null).request.url.query[].disabled) = true' \
-#   intermediateCollectionWrapped.json > intermediateCollectionDisableQueryParam.json
-
-if jq -e '.. | select(.request? != null).request.url.query[] | select(.disabled == true)' intermediateCollectionDisableQueryParam.json > /dev/null; then
+echo "Disabling query params by default test"
+if jq -e '.. | select(.request? != null).request.url.query[] | select(.disabled == true)' $COLLECTION_TRANSFORMED_FILE_NAME > /dev/null; then
   echo "Test Passed: Query params disabled successfully."
 else
   echo "Test Failed: Query params disabling failed."
 fi
 
 # Test: Remove _postman_id
-# echo "Removing _postman_id"
-# jq 'del(.collection.info._postman_id)' \
-#   intermediateCollectionDisableQueryParam.json > intermediateCollectionNoPostmanID.json
-
-if ! jq -e '.collection.info._postman_id' intermediateCollectionNoPostmanID.json > /dev/null; then
+if ! jq -e '.collection.info._postman_id' $COLLECTION_TRANSFORMED_FILE_NAME > /dev/null; then
   echo "Test Passed: _postman_id removed successfully."
 else
   echo "Test Failed: _postman_id removal failed."
 fi
 
-# Test: Remove circular references
-echo "Removing circular references"
-sed 's/\\"value\\": \\"<Circular reference to #[^>"]* detected>\\"//g' intermediateCollectionNoPostmanID.json > intermediateCollectionNoCircular.json
-
-if ! grep -q '<Circular reference to #' intermediateCollectionNoCircular.json; then
-  echo "Test Passed: Circular references removed successfully."
-else
-  echo "Test Failed: Circular references removal failed."
-fi
-
-# Test: Remove all variables
-echo "Removing all variables. We use environment for variables instead"
-jq '.collection.variable = []' \
-  intermediateCollectionWithDescription.json > intermediateCollectionWithNoVarjson.json
-
-if jq -e '.collection.variable | length == 0' intermediateCollectionWithNoVarjson.json > /dev/null; then
-  echo "Test Passed: Variables removed successfully."
-else
-  echo "Test Failed: Variables removal failed."
-fi
-
 # Test : Add baseUrl property
-echo "Adding baseUrl property $BASE_URL"
-jq --arg base_url "$BASE_URL" \
-  '.collection.variable[0].value = $base_url' \
-  intermediateCollectionWithNoVarjson.json > intermediateCollectionWithBaseURL.json
-
-if jq -e --arg base_url "$BASE_URL" '.collection.variable[0].value == $base_url' intermediateCollectionWithBaseURL.json > /dev/null; then
-  echo "Test Passed: baseUrl property added successfully."
+echo "Adding baseUrl property test"
+if jq -e '.collection.variable[0] | (.key == "baseUrl" and has("value"))' $COLLECTION_TRANSFORMED_FILE_NAME > /dev/null; then
+  echo "Test Passed: The first item in the variable array has key set to \"baseUrl\" and has a value property."
 else
-  echo "Test Failed: baseUrl property addition failed."
+  echo "Test Failed: The first item in the variable array does not have the expected key or value properties."
 fi
 
-# End of test script
