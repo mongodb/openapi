e118927b654fa769f92816e65241eba01bd41755
Wojciech Trocki <w.trocki@mongodb.com>
2024-12-09 11:12:01 +0100

    fix: shell checks

--- tools/postman/scripts/transform-for-api-test.sh
+++ tools/postman/scripts/transform-for-api-test.sh
@@ -20,14 +20,14 @@ fi
 
 # Test: Disable query params by default
 echo "Disabling query params by default test"
-if jq -e '.. | select(.request? != null).request.url.query[] | select(.disabled == true)' $COLLECTION_TRANSFORMED_FILE_NAME > /dev/null; then
+if jq -e '.. | select(.request? != null).request.url.query[] | select(.disabled == true)' "$COLLECTION_TRANSFORMED_FILE_NAME" > /dev/null; then
   echo "Test Passed: Query params disabled successfully."
 else
   echo "Test Failed: Query params disabling failed."
 fi
 
 # Test: Remove _postman_id
-if ! jq -e '.collection.info._postman_id' $COLLECTION_TRANSFORMED_FILE_NAME > /dev/null; then
+if ! jq -e '.collection.info._postman_id' "$COLLECTION_TRANSFORMED_FILE_NAME" > /dev/null; then
   echo "Test Passed: _postman_id removed successfully."
 else
   echo "Test Failed: _postman_id removal failed."
@@ -35,7 +35,7 @@ fi
 
 # Test : Add baseUrl property
 echo "Adding baseUrl property test"
-if jq -e '.collection.variable[0] | (.key == "baseUrl" and has("value"))' $COLLECTION_TRANSFORMED_FILE_NAME > /dev/null; then
+if jq -e '.collection.variable[0] | (.key == "baseUrl" and has("value"))' "$COLLECTION_TRANSFORMED_FILE_NAME" > /dev/null; then
   echo "Test Passed: The first item in the variable array has key set to \"baseUrl\" and has a value property."
 else
   echo "Test Failed: The first item in the variable array does not have the expected key or value properties."
