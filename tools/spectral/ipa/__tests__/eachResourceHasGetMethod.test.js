89b16b060aa1a5115c24aaa79c26c90441f39883
Lovisa Berggren <59226031+lovisaberggren@users.noreply.github.com>
2024-12-16 15:10:29 +0000

    CLOUDP-285964: Adds IPA rule 104 - Resource has get (#301)

---
+++ tools/spectral/ipa/__tests__/eachResourceHasGetMethod.test.js
@@ -0,0 +1,128 @@
+import testRule from './__helpers__/testRule';
+import { DiagnosticSeverity } from '@stoplight/types';
+
+testRule('xgen-IPA-104-resource-has-GET', [
+  {
+    name: 'valid methods',
+    document: {
+      paths: {
+        '/standard': {
+          post: {},
+          get: {},
+        },
+        '/standard/{exampleId}': {
+          get: {},
+          patch: {},
+          delete: {},
+        },
+        '/standard/{exampleId}/nested': {
+          post: {},
+          get: {},
+        },
+        '/standard/{exampleId}/nested/{exampleId}': {
+          get: {},
+          patch: {},
+          delete: {},
+        },
+        '/standard/{exampleId}/nestedSingleton': {
+          get: {},
+          patch: {},
+        },
+        '/custom': {
+          post: {},
+          get: {},
+        },
+        '/custom/{exampleId}': {
+          get: {},
+          patch: {},
+          delete: {},
+        },
+        '/custom/{exampleId}:method': {
+          post: {},
+        },
+        '/custom:method': {
+          post: {},
+        },
+        '/singleton': {
+          get: {},
+        },
+      },
+    },
+    errors: [],
+  },
+  {
+    name: 'invalid methods',
+    document: {
+      paths: {
+        '/standard': {
+          post: {},
+          get: {},
+        },
+        '/standard/{exampleId}': {
+          patch: {},
+          delete: {},
+        },
+        '/standard/{exampleId}/nested': {
+          post: {},
+          get: {},
+        },
+        '/standard/{exampleId}/nested/{exampleId}': {
+          patch: {},
+          delete: {},
+        },
+        '/standard/{exampleId}/nestedSingleton': {
+          patch: {},
+        },
+        '/custom': {
+          post: {},
+          get: {},
+        },
+        '/custom/{exampleId}': {
+          patch: {},
+          delete: {},
+        },
+        '/custom/{exampleId}:method': {
+          post: {},
+        },
+        '/custom:method': {
+          post: {},
+        },
+        '/singleton': {
+          patch: {},
+        },
+      },
+    },
+    errors: [
+      {
+        code: 'xgen-IPA-104-resource-has-GET',
+        message: 'APIs must provide a get method for resources. http://go/ipa/117',
+        path: ['paths', '/standard'],
+        severity: DiagnosticSeverity.Warning,
+      },
+      {
+        code: 'xgen-IPA-104-resource-has-GET',
+        message: 'APIs must provide a get method for resources. http://go/ipa/117',
+        path: ['paths', '/standard/{exampleId}/nested'],
+        severity: DiagnosticSeverity.Warning,
+      },
+      {
+        code: 'xgen-IPA-104-resource-has-GET',
+        message: 'APIs must provide a get method for resources. http://go/ipa/117',
+        path: ['paths', '/standard/{exampleId}/nestedSingleton'],
+        severity: DiagnosticSeverity.Warning,
+      },
+      {
+        code: 'xgen-IPA-104-resource-has-GET',
+        message: 'APIs must provide a get method for resources. http://go/ipa/117',
+        path: ['paths', '/custom'],
+        severity: DiagnosticSeverity.Warning,
+      },
+      {
+        code: 'xgen-IPA-104-resource-has-GET',
+        message: 'APIs must provide a get method for resources. http://go/ipa/117',
+        path: ['paths', '/singleton'],
+        severity: DiagnosticSeverity.Warning,
+      },
+    ],
+  },
+]);
