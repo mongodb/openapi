f3ae4cc7d767e6f0fb89eb0c21390cd3d83102e4
Yeliz Henden <yeliz.henden@mongodb.com>
2024-12-17 16:18:11 +0000

    CLOUDP-287245: IPA-109: Validate custom method must be GET or POST

---
+++ tools/spectral/ipa/rulesets/functions/eachCustomMethodMustBeGetOrPost.js
@@ -0,0 +1,25 @@
+import { isCustomMethod } from './utils/resourceEvaluation';
+
+const ERROR_MESSAGE = 'The HTTP method for custom methods must be GET or POST.';
+const ERROR_RESULT = [{ message: ERROR_MESSAGE }];
+const validMethods = ["get", "post"];
+
+export default (input, _, { documentInventory }) => {
+  if(!isCustomMethod(input)) {
+    return;
+  }
+
+  const oas = documentInventory.resolved;
+  let httpMethods = Object.keys(oas.paths[input]);
+
+  const invalidMethodsFound = httpMethods.some((key) => !validMethods.includes(key));
+  if(invalidMethodsFound) {
+    return ERROR_RESULT;
+  }
+
+  const validMethodsFound = httpMethods.filter((key) => validMethods.includes(key));
+  if(validMethodsFound.length > 1) {
+    return ERROR_RESULT;
+  }
+
+}
