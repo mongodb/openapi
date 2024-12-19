619c8c62d31e4f5648aab4868fbee56ee6812274
Yeliz Henden <yeliz.henden@mongodb.com>
2024-12-19 16:24:09 +0000

    address the comments

--- tools/spectral/ipa/rulesets/functions/eachCustomMethodMustUseCamelCase.js
+++ tools/spectral/ipa/rulesets/functions/eachCustomMethodMustUseCamelCase.js
@@ -4,7 +4,7 @@ import { casing } from '@stoplight/spectral-functions';
 
 const RULE_NAME = 'xgen-IPA-109-custom-method-must-use-camel-case';
 const ERROR_MESSAGE = 'The custom method name must use camelCase format.';
-const ERROR_RESULT = [{ message: ERROR_MESSAGE }];
+
 
 export default (input, opts, { path }) => {
   // Extract the path key (e.g., '/a/{exampleId}:method') from the JSONPath.
@@ -17,12 +17,12 @@ export default (input, opts, { path }) => {
   }
 
   let methodName = getCustomMethodName(pathKey);
-  if (!methodName) {
-    return ERROR_RESULT;
+  if (methodName.length === 0 || methodName.trim().length === 0) {
+    return [{ message: "Custom method name cannot be empty or blank." }];
   }
 
   const isCamelCase = casing(methodName, { type: 'camel', disallowDigits: true });
   if (isCamelCase) {
-    return ERROR_RESULT;
+    return [{ message: `${ERROR_MESSAGE} Method name: ${methodName}.` }];
   }
 };
