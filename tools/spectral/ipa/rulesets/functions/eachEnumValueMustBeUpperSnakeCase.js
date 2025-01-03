ade12ad92c26cf85fb8919406ef905e991c3726b
Yeliz Henden <yeliz.henden@mongodb.com>
2025-01-03 15:06:58 +0000

    fix

--- tools/spectral/ipa/rulesets/functions/eachEnumValueMustBeUpperSnakeCase.js
+++ tools/spectral/ipa/rulesets/functions/eachEnumValueMustBeUpperSnakeCase.js
@@ -1,13 +1,17 @@
 import { hasException } from './utils/exceptions.js';
-import { getSchemaPath, resolveObject } from './utils/componentUtils.js';
+import { resolveObject } from './utils/componentUtils.js';
 import { casing } from '@stoplight/spectral-functions';
 
 const RULE_NAME = 'xgen-IPA-123-enum-values-must-be-upper-snake-case';
 const ERROR_MESSAGE = 'enum value must be UPPER_SNAKE_CASE.';
 
+export function getSchemaPathFromEnumPath(path) {
+  return path.slice(0, path.length - 1);
+}
+
 export default (input, _, { path, documentInventory }) => {
   const oas = documentInventory.resolved;
-  const schemaPath = getSchemaPath(path);
+  const schemaPath = getSchemaPathFromEnumPath(path);
   const schemaObject = resolveObject(oas, schemaPath);
   if (hasException(schemaObject, RULE_NAME)) {
     return;
