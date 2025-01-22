f0e22bad0e7c64981172ed197729ac00b2b20efb
Lovisa Berggren <59226031+lovisaberggren@users.noreply.github.com>
2025-01-22 15:40:41 +0000

    CLOUDP-295956: Refactor + add testing for IPA metrics collection (#383)

--- tools/spectral/ipa/metrics/config.js
+++ tools/spectral/ipa/metrics/config.js
@@ -7,12 +7,10 @@ const rootDir = path.resolve(dirname, '../../../../');
 const config = {
   defaultOasFilePath: path.join(rootDir, 'openapi', 'v2.json'),
   defaultRulesetFilePath: path.join(dirname, '..', 'ipa-spectral.yaml'),
-  defaultCollectorResultsFilePath: path.join(dirname, 'ipa-collector-results-combined.log'),
+  defaultCollectorResultsFilePath: path.join(dirname, 'scripts', 'ipa-collector-results-combined.log'),
   defaultOutputsDir: path.join(dirname, 'outputs'),
 };
 
 config.defaultMetricCollectionResultsFilePath = path.join(config.defaultOutputsDir, 'metric-collection-results.json');
-config.defaultSpectralReportFile = path.join(config.defaultOutputsDir, 'spectral-report.xml');
-config.defaultSpectralOutputFile = path.join(config.defaultOutputsDir, 'spectral-output.txt');
 
 export default config;
---
+++ tools/spectral/ipa/metrics/scripts/runMetricCollection.js
@@ -0,0 +1,35 @@
+import fs from 'node:fs';
+import { spawnSync } from 'child_process';
+import spectral from '@stoplight/spectral-core';
+import config from '../config.js';
+import { runMetricCollectionJob } from '../metricCollection.js';
+
+const { Spectral } = spectral;
+const args = process.argv.slice(2);
+const oasFilePath = args[0];
+
+if (!fs.existsSync(config.defaultOutputsDir)) {
+  fs.mkdirSync(config.defaultOutputsDir);
+  console.log(`Output directory created successfully`);
+}
+
+const result = spawnSync('spectral', ['lint', config.defaultOasFilePath, '--ruleset', config.defaultRulesetFilePath]);
+
+if (result.error) {
+  console.error('Error running Spectral lint:', result.error);
+  process.exit(1);
+}
+
+console.log('Spectral lint completed successfully.');
+
+runMetricCollectionJob(
+  {
+    oasFilePath,
+  },
+  new Spectral()
+)
+  .then((results) => {
+    console.log('Writing results');
+    fs.writeFileSync(config.defaultMetricCollectionResultsFilePath, JSON.stringify(results));
+  })
+  .catch((error) => console.error(error.message));
