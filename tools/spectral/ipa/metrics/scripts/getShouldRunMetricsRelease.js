fa602c37a9ffdcda78f17611875ede84a2f80810
Lovisa Berggren <lovisa.berggren@mongodb.com>
2025-01-23 12:25:26 +0000

    CLOUDP-290416: Add workflow for releasing IPA metrics

---
+++ tools/spectral/ipa/metrics/scripts/getShouldRunMetricsRelease.js
@@ -0,0 +1,31 @@
+// Used in .github/workflows/release-IPA-metrics.yml
+export default async function getShouldRunMetricsRelease({ github, context }) {
+  const response = await github.actions.listWorkflowRuns({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    workflow_id: context.workflow,
+    per_page: 2,
+    page: 1,
+  });
+
+  if (response === undefined) {
+    return true;
+  }
+
+  const { data: runs } = response;
+
+  if (runs === undefined || runs.length === 0) {
+    return true;
+  }
+
+  const previousStatus = runs[1].status;
+
+  const lastRunDate = new Date(runs[1].created_at);
+  const today = new Date();
+  const lastRunWasToday =
+    today.getFullYear() === lastRunDate.getFullYear() &&
+    today.getMonth() === lastRunDate.getMonth() &&
+    today.getDate() === lastRunDate.getDate();
+
+  return previousStatus === 'failure' || !lastRunWasToday;
+}
