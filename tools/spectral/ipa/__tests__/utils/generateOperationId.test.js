5862b50488cb6b88f45ed33d292d4be9d6f93419
Lovisa Berggren <lovisa.berggren@mongodb.com>
2025-02-05 16:51:53 +0000

    CLOUDP-297705: POC generate operation ID from method+path

---
+++ tools/spectral/ipa/__tests__/utils/generateOperationId.test.js
@@ -0,0 +1,119 @@
+import { describe, expect, it } from '@jest/globals';
+import {
+  generateOperationIdForCustomMethod,
+  generateOperationIdForStandardMethod,
+} from '../../rulesets/functions/utils/generateOperationId.js';
+
+const customMethodCases = [
+  // Real examples that work well
+  {
+    path: '/api/atlas/v2/orgs/{orgId}/resourcePolicies:validate',
+    expectedOperationId: 'validateOrgResourcePolicies',
+  },
+  {
+    path: '/api/atlas/v2/orgs/{orgId}/invoices/{invoiceId}/lineItems:search',
+    expectedOperationId: 'searchOrgInvoiceLineItems',
+  },
+  {
+    path: '/api/atlas/v2/groups/{groupId}:migrate',
+    expectedOperationId: 'migrateGroup',
+  },
+  {
+    path: '/api/atlas/v2/groups/{groupId}/serviceAccounts/{clientId}:invite',
+    expectedOperationId: 'inviteGroupServiceAccountsClient',
+  },
+  {
+    path: '/api/atlas/v2/groups/{groupId}/streams/{tenantName}/processor/{processorName}:stop',
+    expectedOperationId: 'stopGroupStreamsTenantProcessor',
+  },
+  {
+    path: '/api/atlas/v2/groups/{groupId}/flexClusters:tenantUpgrade',
+    expectedOperationId: 'tenantGroupFlexClustersUpgrade',
+  },
+  {
+    path: '/api/atlas/v2/orgs/{orgId}/users/{userId}:addRole',
+    expectedOperationId: 'addOrgUserRole',
+  },
+  // Some examples to show some edge cases
+  {
+    path: '/api/atlas/v2/orgs/{orgId}/billing/costExplorer:getUsage',
+    expectedOperationId: 'getOrgBillingCostExplorerUsage', // Double parent case works well here
+  },
+  // Some examples to show some caveats
+  {
+    path: '/api/atlas/v2/groups/{groupId}/streams:withSampleConnections',
+    method: 'get',
+    expectedOperationId: 'withGroupStreamsSampleConnections', // This one has a weird custom method, ideally it would be /streams:createWithSampleConnections
+  },
+];
+
+const standardMethodCases = [
+  // Real examples that work well
+  {
+    path: '/api/atlas/v2/groups/{groupId}/serviceAccounts',
+    method: 'list',
+    expectedOperationId: 'listGroupServiceAccounts',
+  },
+  {
+    path: '/api/atlas/v2/groups/{groupId}/serviceAccounts/{clientId}',
+    method: 'get',
+    expectedOperationId: 'getGroupServiceAccountsClient',
+  },
+  {
+    path: '/api/atlas/v2/groups/{groupId}/pushBasedLogExport',
+    method: 'delete',
+    expectedOperationId: 'deleteGroupPushBasedLogExport',
+  },
+  {
+    path: '/api/atlas/v2/groups/{groupId}/processes/{processId}/measurements',
+    method: 'list',
+    expectedOperationId: 'listGroupProcessMeasurements',
+  },
+  // Some examples to show some caveats
+  {
+    path: '/api/atlas/v2/groups/{groupId}/serviceAccounts',
+    method: 'create',
+    expectedOperationId: 'createGroupServiceAccounts', // Ideally singular instead of plural
+  },
+  {
+    path: '/api/atlas/v2/groups/{groupId}/serverless/{name}',
+    method: 'delete',
+    expectedOperationId: 'deleteGroupServerlessName', // Ideally it should be something like /{instanceName} -> deleteGroupServerlessInstance
+  },
+  {
+    path: '/api/atlas/v2/groups/{groupId}/cloudProviderAccess/{cloudProvider}/{roleId}',
+    method: 'get',
+    expectedOperationId: 'getGroupCloudRole', // Ideally the provider from cloudProvider wouldn't be stripped here
+  },
+  {
+    path: '/api/atlas/v2/orgs',
+    method: 'list',
+    expectedOperationId: 'listOrgs',
+  },
+  {
+    path: '/api/atlas/v2/orgs/{orgId}/apiKeys/{apiUserId}',
+    method: 'get',
+    expectedOperationId: 'getOrgApiKeysApiUser', // This one is a bit weird, ideally it would be /apiKeys/{apiKeyId} -> getOrgApiKey
+  },
+  {
+    path: '/api/atlas/v2/groups/{groupId}/privateEndpoint/{cloudProvider}/endpointService/{endpointServiceId}/endpoint/{endpointId}',
+    method: 'delete',
+    expectedOperationId: 'deleteGroupPrivateEndpointCloudEndpointServiceEndpoint', // This gets complicated, and ideally for this case cloudProvider wouldn't be stripped to only 'cloud'
+  },
+];
+
+describe('tools/spectral/ipa/rulesets/functions/utils/generateOperationId.js', () => {
+  for (const testCase of customMethodCases) {
+    it.concurrent(`Custom method ${testCase.path} gets operationId ${testCase.expectedOperationId}`, () => {
+      expect(generateOperationIdForCustomMethod(testCase.path)).toBe(testCase.expectedOperationId);
+    });
+  }
+  for (const testCase of standardMethodCases) {
+    it.concurrent(
+      `Standard method ${testCase.method} ${testCase.path} gets operationId ${testCase.expectedOperationId}`,
+      () => {
+        expect(generateOperationIdForStandardMethod(testCase.path, testCase.method)).toBe(testCase.expectedOperationId);
+      }
+    );
+  }
+});
---
+++ tools/spectral/ipa/rulesets/functions/utils/generateOperationId.js
@@ -0,0 +1,148 @@
+const PATH_PREFIX = 'api/atlas/v2/';
+const lowerCasePattern = new RegExp('^[a-z]+$');
+
+// Method should be get, delete, update or create
+export function generateOperationIdForStandardMethod(path, method) {
+  let remainingPath = path.split(PATH_PREFIX)[1].split('/');
+
+  // Start with the method, for example, 'get' or 'list'
+  let operationId = method;
+
+  // Add the rest of the words from the path
+  operationId += getOperationIdFromPathSections(remainingPath);
+
+  return operationId;
+}
+
+export function generateOperationIdForCustomMethod(path) {
+  const resourcePath = path.split(':')[0];
+  const customMethodName = path.split(':')[1];
+
+  let remainingPath = resourcePath.split(PATH_PREFIX)[1].split('/');
+  let operationId = '';
+
+  // Get custom verb to start the operationId
+  // invite -> invite
+  // addNode -> add
+  let customVerb;
+  let remainingCustomMethodName = '';
+  if (lowerCasePattern.test(customMethodName)) {
+    customVerb = customMethodName;
+  } else {
+    customVerb = getFirstWordFromCamelCase(customMethodName);
+    remainingCustomMethodName = customMethodName.substring(customVerb.length);
+  }
+  operationId += customVerb;
+
+  operationId += getOperationIdFromPathSections(remainingPath);
+
+  // Add any remaining words from the custom name to the end
+  // /orgs/{orgId}/users/{userId}:addRole -> add + Org + User + Role
+  operationId += remainingCustomMethodName;
+
+  return operationId;
+}
+
+function getOperationIdFromPathSections(remainingPath) {
+  // Get resource names along the path and add to operationId
+  // /orgs/{orgId}/users/{userId} -> Org + User
+  // /groups/{groupId}/flexCluster -> Group + FlexCluster
+  let operationId = '';
+  while (remainingPath.length > 0) {
+    const { nextWord, strippedPath } = getWordFromNextResource(remainingPath);
+    operationId += capitalizeFirstLetter(nextWord);
+    remainingPath = strippedPath;
+  }
+  return operationId;
+}
+
+function getWordFromNextResource(pathSections) {
+  // If there is a parent + child
+  if (pathSections.length > 1 && !pathSections[0].startsWith('{') && pathSections[1].startsWith('{')) {
+    const parentResource = pathSections[0];
+    // If parent ant specifier does not start the same way, return both
+    // For example ServiceAccounts + Client
+    const specifier = getResourceNameFromResourceSpecifier(pathSections[1]);
+    if (!parentResource.startsWith(specifier)) {
+      const nextWord = parentResource + capitalizeFirstLetter(specifier);
+      const strippedPath = pathSections.slice(2);
+      return { nextWord, strippedPath };
+    }
+    // If parent and specifier starts the same way, for example org + orgId
+    // Return only specifier, in this case org
+    const nextWord = specifier;
+    const strippedPath = pathSections.slice(2);
+    return { nextWord, strippedPath };
+  }
+  // If next path is a child, strip brackets and return resource name from specifier
+  if (pathSections[0].startsWith('{')) {
+    return {
+      nextWord: getResourceNameFromResourceSpecifier(pathSections[0]),
+      strippedPath: pathSections.slice(1),
+    };
+  }
+  // Else, just return next word
+  return {
+    nextWord: pathSections[0],
+    strippedPath: pathSections.slice(1),
+  };
+}
+
+/**
+ * Returns the resource name from a resource specifier.
+ * For example, '{orgId}' returns 'org', 'apiUserId' returns 'apiUser'
+ *
+ * @param resourceSpecifier the resource specifier, including brackets
+ * @returns {string} the resource name derived from the specifier
+ */
+function getResourceNameFromResourceSpecifier(resourceSpecifier) {
+  const strippedFromBrackets = stripStringFromBrackets(resourceSpecifier);
+  if (lowerCasePattern.test(strippedFromBrackets)) {
+    return strippedFromBrackets;
+  }
+  return removeLastWordFromCamelCase(strippedFromBrackets);
+}
+
+/**
+ * Strips a string from surrounding curly brackets, if there are no brackets the function just returns the string.
+ *
+ * @param string the string to remove the curly brackets from
+ * @returns {string} the string without the brackets
+ */
+function stripStringFromBrackets(string) {
+  if (string.startsWith('{') && string.endsWith('}')) {
+    return string.substring(1, string.length - 1);
+  }
+  return string;
+}
+
+/**
+ * Returns the first word from a camelCase string, for example, 'camelCase' returns 'camel'.
+ *
+ * @param string the string to get the first word from
+ * @returns {string} the first word from the passed string
+ */
+function getFirstWordFromCamelCase(string) {
+  return string.split(/(?=[A-Z])/)[0];
+}
+
+/**
+ * Removed the last word from a camelCase string, for example, 'camelCaseWord' returns 'camelCase'.
+ *
+ * @param string the string to get the first word from
+ * @returns {string} the first word from the passed string
+ */
+function removeLastWordFromCamelCase(string) {
+  const words = string.split(/(?=[A-Z][^A-Z]+$)/);
+  return words.slice(0, words.length - 1).join();
+}
+
+/**
+ * Capitalizes the first letter in a string.
+ *
+ * @param string
+ * @returns {string}
+ */
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
