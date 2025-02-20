c3c3096b55b20de0a4f8ad400f8fb172bd7a1918
Bianca Lisle <40155621+blva@users.noreply.github.com>
2025-02-20 18:25:10 +0000

    chore: update script  (#439)

--- .github/scripts/split_spec.sh
+++ .github/scripts/split_spec.sh
@@ -2,7 +2,7 @@
 set -eou pipefail
 
 echo "Running FOAS CLI versions command"
-foascli versions -s openapi-foas.json -o ./openapi/v2/versions.json --env "${target_env:?}" --stability-level stable
+foascli versions -s openapi-foas.json -o ./openapi/v2/versions.json --env "${target_env:?}" --stability-level stable  --stability-level preview
 
 echo "Running FOAS CLI split command with the following --env=${target_env:?} and -o=./openapi/v2/openapi.json"
 
@@ -11,6 +11,9 @@ mv -f "openapi-foas.json" "./openapi/v2.json"
 
 foascli split -s openapi-foas.yaml --env "${target_env:?}" -o ./openapi/v2/openapi.yaml
 mv -f "openapi-foas.yaml" "./openapi/v2.yaml"
+
+# Create folder if it does not exist
+mkdir -p ./openapi/private
 
 echo "Moving preview files to preview and private-preview folder"
 find ./openapi/v2 -type f -name "*private-preview*" -exec mv -f {} ./openapi/private/ \;
