6bde71ac6bb5ede3792b092db283fde0b937d5cf
Bianca Lisle <40155621+blva@users.noreply.github.com>
2025-02-20 15:11:00 +0000

    CLOUDP-301106: Move preview specs to preview folder (#433)

--- .github/scripts/split_spec.sh
+++ .github/scripts/split_spec.sh
@@ -2,7 +2,7 @@
 set -eou pipefail
 
 echo "Running FOAS CLI versions command"
-foascli versions -s openapi-foas.json -o ./openapi/v2/versions.json --env "${target_env:?}"
+foascli versions -s openapi-foas.json -o ./openapi/v2/versions.json --env "${target_env:?}" --stability-level stable
 
 echo "Running FOAS CLI split command with the following --env=${target_env:?} and -o=./openapi/v2/openapi.json"
 
@@ -11,3 +11,6 @@ mv -f "openapi-foas.json" "./openapi/v2.json"
 
 foascli split -s openapi-foas.yaml --env "${target_env:?}" -o ./openapi/v2/openapi.yaml
 mv -f "openapi-foas.yaml" "./openapi/v2.yaml"
+
+echo "Moving preview files to preview and private-preview folder"
+find ./openapi/v2 -type f -name "*private-preview*" -exec mv -f {} ./openapi/private/ \;
