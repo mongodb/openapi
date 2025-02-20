f84b446e41b124260cc925d6bc211cf728e90899
Bianca Lisle <bianca.vianadeaguiar@mongodb.com>
2025-02-20 14:58:55 +0000

    move only private-preview files

--- .github/scripts/split_spec.sh
+++ .github/scripts/split_spec.sh
@@ -4,9 +4,6 @@ set -eou pipefail
 echo "Running FOAS CLI versions command"
 foascli versions -s openapi-foas.json -o ./openapi/v2/versions.json --env "${target_env:?}" --stability-level stable
 
-echo "Running FOAS CLI versions command for preview"
-foascli versions -s openapi-foas.json -o ./openapi/preview/versions.json --env "${target_env:?}" --stability-level preview
-
 echo "Running FOAS CLI split command with the following --env=${target_env:?} and -o=./openapi/v2/openapi.json"
 
 foascli split -s openapi-foas.json --env "${target_env:?}" -o ./openapi/v2/openapi.json
@@ -16,5 +13,4 @@ foascli split -s openapi-foas.yaml --env "${target_env:?}" -o ./openapi/v2/opena
 mv -f "openapi-foas.yaml" "./openapi/v2.yaml"
 
 echo "Moving preview files to preview and private-preview folder"
-find ./openapi/v2 -type f -name "*private-preview*" -exec mv -f {} ./openapi/private-preview/ \;
-find ./openapi/v2 -type f -name "*preview*" -exec mv -f {} ./openapi/preview/ \;
+find ./openapi/v2 -type f -name "*private-preview*" -exec mv -f {} ./openapi/private/ \;
