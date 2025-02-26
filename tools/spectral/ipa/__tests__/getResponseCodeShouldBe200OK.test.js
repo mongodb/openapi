0f7c74cbaa9b57becdbdee01084ca6c9b1aa9907
Yeliz Henden <yeliz.henden@mongodb.com>
2025-02-26 18:07:22 +0000

    CLOUDP-271991: IPA-104: Validate for Get methods the response is 200

---
+++ tools/spectral/ipa/__tests__/getResponseCodeShouldBe200OK.test.js
@@ -0,0 +1,100 @@
+import testRule from './__helpers__/testRule';
+import { DiagnosticSeverity } from '@stoplight/types';
+
+testRule('xgen-IPA-104-GET-response-code-should-be-200-OK', [
+  {
+    name: 'valid methods',
+    document: {
+      paths: {
+        '/path1/{resource}': {
+          get: {
+            responses: {
+              '200': {},
+              '400': {},
+              '500': {},
+            },
+          },
+        },
+        '/path2/{resource}': {
+          get: {
+            responses: {
+              '200': {},
+              '401': {},
+              '404': {},
+            },
+          },
+        },
+      },
+    },
+    errors: [],
+  },
+  {
+    name: 'invalid methods',
+    document: {
+      paths: {
+        '/path1/{resource}': {
+          get: {
+            responses: {
+              '201': {},
+              '400': {},
+              '500': {},
+            },
+          },
+        },
+        '/path2/{resource}': {
+          get: {
+            responses: {
+              '400': {},
+              '500': {},
+            },
+          },
+        },
+      },
+    },
+    errors: [
+      {
+        code: 'xgen-IPA-104-GET-response-code-should-be-200-OK',
+        message: 'GET method response code should be 200 OK. http://go/ipa/104',
+        path: ['paths', '/path1/{resource}'],
+        severity: DiagnosticSeverity.Warning,
+      },
+      {
+        code: 'xgen-IPA-104-GET-response-code-should-be-200-OK',
+        message: 'GET method response code should be 200 OK. http://go/ipa/104',
+        path: ['paths', '/path2/{resource}'],
+        severity: DiagnosticSeverity.Warning,
+      },
+    ],
+  },
+  {
+    name: 'invalid method with exception',
+    document: {
+      paths: {
+        '/path1/{resource}': {
+          get: {
+            responses: {
+              '201': {},
+              '400': {},
+              '500': {},
+            },
+          },
+          'x-xgen-IPA-exception': {
+            'xgen-IPA-104-GET-response-code-should-be-200-OK': 'reason',
+          },
+        },
+        '/path2/{resource}': {
+          get: {
+            responses: {
+              '400': {},
+              '500': {},
+            },
+          },
+          'x-xgen-IPA-exception': {
+            'xgen-IPA-104-GET-response-code-should-be-200-OK': 'reason',
+          },
+        },
+      },
+    },
+    errors: [],
+  },
+]);
---
+++ tools/spectral/ipa/rulesets/functions/getResponseCodeShouldBe200OK.js
@@ -0,0 +1,29 @@
+import { hasException } from './utils/exceptions.js';
+import { collectAdoption, collectAndReturnViolation, collectException } from './utils/collectionUtils.js';
+
+const RULE_NAME = 'xgen-IPA-104-GET-response-code-should-be-200-OK';
+const ERROR_MESSAGE = 'GET method response code should be 200 OK.';
+
+export default (input, _, { path, documentInventory }) => {
+
+  const oas = documentInventory.resolved;
+
+  if (hasException(oas.paths[input], RULE_NAME)) {
+    collectException(oas.paths[input], RULE_NAME, path);
+    return;
+  }
+
+  let pathItem = oas.paths[input];
+  if(!pathItem.get) {
+    return;
+  }
+
+  if(pathItem.get && pathItem.get.responses) {
+    const responses = pathItem.get.responses;
+    if(!responses['200']) {
+      return collectAndReturnViolation(path, RULE_NAME, ERROR_MESSAGE);
+    }
+  }
+
+  collectAdoption(path, RULE_NAME);
+};
