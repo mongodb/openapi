d6af94fbf5395c778f41629a6196e5d7b0ecd857
Yeliz Henden <yeliz.henden@mongodb.com>
2025-02-27 16:09:25 +0000

    fix

--- tools/spectral/ipa/rulesets/functions/getResponseCodeShouldBe200OK.js
+++ tools/spectral/ipa/rulesets/functions/getResponseCodeShouldBe200OK.js
@@ -1,20 +1,19 @@
 import { hasException } from './utils/exceptions.js';
 import { collectAdoption, collectAndReturnViolation, collectException } from './utils/collectionUtils.js';
-import { resolveObject } from './utils/componentUtils.js';
 
 const RULE_NAME = 'xgen-IPA-104-GET-response-code-should-be-200-OK';
 const ERROR_MESSAGE = 'The HTTP response status code for GET operations should be 200 OK.';
 
-export default (input, _, { path, documentInventory }) => {
-  const oas = documentInventory.resolved;
-  const getOperation = resolveObject(oas, path);
-  if (hasException(getOperation, RULE_NAME)) {
-    collectException(getOperation, RULE_NAME, path);
+export default (input, _, { path }) => {
+  console.log(input);
+
+  if (hasException(input, RULE_NAME)) {
+    collectException(input, RULE_NAME, path);
     return;
   }
 
-  if (getOperation.responses) {
-    const responses = getOperation.responses;
+  if (input['responses']) {
+    const responses = input['responses'];
     if (!responses['200']) {
       return collectAndReturnViolation(path, RULE_NAME, ERROR_MESSAGE);
     }
