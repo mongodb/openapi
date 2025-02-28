7779268fd843bab731c86f930ff5c4df8943a347
Lovisa Berggren <lovisa.berggren@mongodb.com>
2025-02-28 15:05:33 +0000

    CLOUDP-302982: IPA-104 - Get object should have suffix "Response"

---
+++ tools/spectral/ipa/rulesets/functions/getMethodReturnsResponseSuffixedObject.js
@@ -0,0 +1,40 @@
+import { isChild, isCustomMethod, isSingletonResource, getResourcePaths } from './utils/resourceEvaluation.js';
+import { getAllSuccessfulResponseSchemaNames } from './utils/methodUtils.js';
+import { resolveObject } from './utils/componentUtils.js';
+import { hasException } from './utils/exceptions.js';
+import { collectAdoption, collectAndReturnViolation, collectException } from './utils/collectionUtils.js';
+
+const RULE_NAME = 'xgen-IPA-104-get-method-returns-response-suffixed-object';
+const ERROR_MESSAGE = 'schema name should end in "Response".';
+
+export default (input, _, { path, document }) => {
+  const resourcePath = path[1];
+  const oas = document.data;
+  const resourcePaths = getResourcePaths(resourcePath, Object.keys(oas.paths));
+
+  if (isCustomMethod(resourcePath) || (!isChild(resourcePath) && !isSingletonResource(resourcePaths))) {
+    return;
+  }
+
+  const errors = [];
+
+  const responseSchemaNames = getAllSuccessfulResponseSchemaNames(oas.paths[resourcePath].get);
+  responseSchemaNames.forEach(({ schemaName, schemaPath }) => {
+    const fullPath = path.concat(schemaPath);
+    const responseObject = resolveObject(oas, fullPath);
+
+    if (hasException(responseObject, RULE_NAME)) {
+      collectException(responseObject, RULE_NAME, fullPath);
+    } else if (!schemaName.endsWith('Response')) {
+      collectAndReturnViolation(fullPath, RULE_NAME, `${schemaName} ${ERROR_MESSAGE}`);
+      errors.push({
+        path: fullPath,
+        message: `${schemaName} ${ERROR_MESSAGE}`,
+      });
+    } else {
+      collectAdoption(fullPath, RULE_NAME);
+    }
+  });
+
+  return errors;
+};
