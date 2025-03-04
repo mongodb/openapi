a32c1520dbc1b34e5806e721f438694ec790bbc3
Yeliz Henden <yeliz.henden@mongodb.com>
2025-03-04 16:54:11 +0000

    inline schemas

--- tools/spectral/ipa/__tests__/createMethodRequestBodyIsRequestSuffixedObject.test.js
+++ tools/spectral/ipa/__tests__/createMethodRequestBodyIsRequestSuffixedObject.test.js
@@ -93,19 +93,6 @@ testRule('xgen-IPA-106-create-method-request-body-is-request-suffixed-object', [
             },
           },
         },
-        '/resource/{id}': {
-          post: {
-            requestBody: {
-              content: {
-                'application/vnd.atlas.2023-01-01+json': {
-                  schema: {
-                    $ref: '#/components/schemas/Schema',
-                  },
-                },
-              },
-            },
-          },
-        },
         '/resource2': {
           post: {
             requestBody: {
@@ -124,6 +111,19 @@ testRule('xgen-IPA-106-create-method-request-body-is-request-suffixed-object', [
             },
           },
         },
+        '/resource3': {
+          post: {
+            requestBody: {
+              content: {
+                'application/vnd.atlas.2023-01-01+json': {
+                  schema: {
+                    type: "object",
+                  },
+                },
+              },
+            },
+          },
+        },
       },
     },
     errors: [
@@ -136,12 +136,6 @@ testRule('xgen-IPA-106-create-method-request-body-is-request-suffixed-object', [
       {
         code: 'xgen-IPA-106-create-method-request-body-is-request-suffixed-object',
         message: 'Response body for the Create method should refer to Request suffixed schema. http://go/ipa/106',
-        path: ['paths', '/resource/{id}', 'post', 'requestBody', 'content', 'application/vnd.atlas.2023-01-01+json'],
-        severity: DiagnosticSeverity.Warning,
-      },
-      {
-        code: 'xgen-IPA-106-create-method-request-body-is-request-suffixed-object',
-        message: 'Response body for the Create method should refer to Request suffixed schema. http://go/ipa/106',
         path: ['paths', '/resource2', 'post', 'requestBody', 'content', 'application/vnd.atlas.2023-01-01+json'],
         severity: DiagnosticSeverity.Warning,
       },
@@ -149,6 +143,12 @@ testRule('xgen-IPA-106-create-method-request-body-is-request-suffixed-object', [
         code: 'xgen-IPA-106-create-method-request-body-is-request-suffixed-object',
         message: 'Response body for the Create method should refer to Request suffixed schema. http://go/ipa/106',
         path: ['paths', '/resource2', 'post', 'requestBody', 'content', 'application/vnd.atlas.2024-01-01+json'],
+        severity: DiagnosticSeverity.Warning,
+      },
+      {
+        code: 'xgen-IPA-106-create-method-request-body-is-request-suffixed-object',
+        message: 'Response body for the Create method should refer to Request suffixed schema. http://go/ipa/106',
+        path: ['paths', '/resource3', 'post', 'requestBody', 'content', 'application/vnd.atlas.2023-01-01+json'],
         severity: DiagnosticSeverity.Warning,
       },
     ],
@@ -166,9 +166,49 @@ testRule('xgen-IPA-106-create-method-request-body-is-request-suffixed-object', [
                   schema: {
                     $ref: '#/components/schemas/Schema',
                   },
+                  'x-xgen-IPA-exception': {
+                    'xgen-IPA-106-create-method-request-body-is-request-suffixed-object': 'reason',
+                  },
                 },
-                'x-xgen-IPA-exception': {
-                  'xgen-IPA-106-create-method-request-body-is-request-suffixed-object': 'reason',
+              },
+            },
+          },
+        },
+        '/resource/{id}': {
+          post: {
+            requestBody: {
+              content: {
+                'application/vnd.atlas.2023-01-01+json': {
+                  schema: {
+                    $ref: '#/components/schemas/Schema',
+                  },
+                  'x-xgen-IPA-exception': {
+                    'xgen-IPA-106-create-method-request-body-is-request-suffixed-object': 'reason',
+                  },
+                },
+              },
+            },
+          },
+        },
+        '/resource2': {
+          post: {
+            requestBody: {
+              content: {
+                'application/vnd.atlas.2023-01-01+json': {
+                  schema: {
+                    $ref: '#/components/schemas/Schema',
+                  },
+                  'x-xgen-IPA-exception': {
+                    'xgen-IPA-106-create-method-request-body-is-request-suffixed-object': 'reason',
+                  },
+                },
+                'application/vnd.atlas.2024-01-01+json': {
+                  schema: {
+                    $ref: '#/components/schemas/Schema',
+                  },
+                  'x-xgen-IPA-exception': {
+                    'xgen-IPA-106-create-method-request-body-is-request-suffixed-object': 'reason',
+                  },
                 },
               },
             },
--- tools/spectral/ipa/rulesets/functions/createMethodRequestBodyIsRequestSuffixedObject.js
+++ tools/spectral/ipa/rulesets/functions/createMethodRequestBodyIsRequestSuffixedObject.js
@@ -9,33 +9,27 @@ const ERROR_MESSAGE = 'Response body for the Create method should refer to Reque
 export default (input, _, { path, documentInventory }) => {
   const oas = documentInventory.unresolved;
   const resourcePath = path[1];
+
   if (isCustomMethod(resourcePath)) {
     return;
   }
 
-  const content = resolveObject(oas, path);
-  if (hasException(content, RULE_NAME)) {
-    collectException(content, RULE_NAME, path);
+  const contentPerMediaType = resolveObject(oas, path);
+
+  if (hasException(contentPerMediaType, RULE_NAME)) {
+    collectException(contentPerMediaType, RULE_NAME, path);
     return;
   }
 
-  const errors = [];
-  for (const mediaType in content) {
-    const media = content[mediaType];
-    if (media.schema) {
-      const schema = media.schema;
-      if (schema.$ref && !schema.$ref.endsWith('Request')) {
-        errors.push({
-          path: path.concat(mediaType),
-          message: `${ERROR_MESSAGE}`,
-        });
-      }
+  if (contentPerMediaType.schema) {
+    const schema = contentPerMediaType.schema;
+    if (schema.$ref && !schema.$ref.endsWith('Request')) {
+      return collectAndReturnViolation(path, RULE_NAME, ERROR_MESSAGE);
+    }
+    if(!schema.$ref) {
+      return collectAndReturnViolation(path, RULE_NAME, ERROR_MESSAGE);
     }
   }
 
-  if (errors.length === 0) {
-    collectAdoption(path, RULE_NAME);
-  } else {
-    return collectAndReturnViolation(path, RULE_NAME, errors);
-  }
+  collectAdoption(path, RULE_NAME);
 };
