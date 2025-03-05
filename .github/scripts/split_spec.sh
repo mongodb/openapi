3d53b560bbd92d24a40dae30cc0cd392402e489d
Bianca Lisle <bianca.vianadeaguiar@mongodb.com>
2025-03-05 18:02:10 +0000

    Update

--- .github/scripts/split_spec.sh
+++ .github/scripts/split_spec.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
+#########################################################
+# Run foas cli to split the openapi file
+# Variables:
+#   target_env - target environment to split the openapi file
+#########################################################
+
 echo "Running FOAS CLI versions command"
 foascli versions -s openapi-foas.json -o ./openapi/v2/versions.json --env "${target_env:?}" --stability-level stable  --stability-level preview
 
 echo "Running FOAS CLI split command with the following --env=${target_env:?} and -o=./openapi/v2/openapi.json"
 
-foascli split -s openapi-foas.json --env "${target_env:?}" -o ./openapi/v2/openapi.json
+foascli split -s openapi-foas.json --env "${target_env:?}" -o ./openapi/v2/openapi.json --format all
 mv -f "openapi-foas.json" "./openapi/v2.json"
-
-foascli split -s openapi-foas.yaml --env "${target_env:?}" -o ./openapi/v2/openapi.yaml
 mv -f "openapi-foas.yaml" "./openapi/v2.yaml"
+
 # Create folder if it does not exist
 mkdir -p ./openapi/v2/private
 
--- tools/cli/internal/cli/split/split.go
+++ tools/cli/internal/cli/split/split.go
@@ -90,8 +90,18 @@ func (o *Opts) saveVersionedOas(oas *openapi3.T, version string) error {
 		path = o.outputPath
 	}
 
-	path = strings.Replace(path, "."+o.format, fmt.Sprintf("-%s.%s", version, o.format), 1)
+	path = getVersionPath(path, version)
 	return openapi.Save(path, oas, o.format, o.fs)
+}
+
+// getVersionPath replaces file path from 'path/path.to.file/file.<json|yaml|any>'
+// to 'path/path.to.file/file-version.<json|yaml|any>'
+func getVersionPath(path, version string) string {
+	extIndex := strings.LastIndex(path, ".")
+	if extIndex == -1 {
+		return fmt.Sprintf("%s-%s", path, version)
+	}
+	return fmt.Sprintf("%s-%s%s", path[:extIndex], version, path[extIndex:])
 }
 
 func (o *Opts) PreRunE(_ []string) error {
@@ -128,7 +138,7 @@ func Builder() *cobra.Command {
 	cmd.Flags().StringVarP(&opts.basePath, flag.Spec, flag.SpecShort, "-", usage.Spec)
 	cmd.Flags().StringVar(&opts.env, flag.Environment, "", usage.Environment)
 	cmd.Flags().StringVarP(&opts.outputPath, flag.Output, flag.OutputShort, "", usage.Output)
-	cmd.Flags().StringVarP(&opts.format, flag.Format, flag.FormatShort, openapi.JSON, usage.Format)
+	cmd.Flags().StringVarP(&opts.format, flag.Format, flag.FormatShort, openapi.ALL, usage.Format)
 	cmd.Flags().StringVar(&opts.gitSha, flag.GitSha, "", usage.GitSha)
 
 	_ = cmd.MarkFlagRequired(flag.Output)
