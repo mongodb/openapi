10faa72ba3d9b599b9b5f9c17aedc27224b4383f
Yeliz Henden <yeliz.henden@mongodb.com>
2025-03-06 11:49:43 +0000

    Remove unresolved oas

--- tools/spectral/ipa/rulesets/functions/createMethodShouldNotHaveQueryParameters.js
+++ tools/spectral/ipa/rulesets/functions/createMethodShouldNotHaveQueryParameters.js
@@ -1,20 +1,18 @@
 import { hasException } from './utils/exceptions.js';
 import { collectAdoption, collectAndReturnViolation, collectException } from './utils/collectionUtils.js';
 import { isCustomMethodIdentifier } from './utils/resourceEvaluation.js';
-import { resolveObject } from './utils/componentUtils.js';
 
 const RULE_NAME = 'xgen-IPA-106-create-method-should-not-have-query-parameters';
 const ERROR_MESSAGE = 'Create operations should not have query parameters.';
 
-export default (input, _, { path, documentInventory }) => {
-  const oas = documentInventory.unresolved;
+export default (input, _, { path }) => {
   const resourcePath = path[1];
 
   if (isCustomMethodIdentifier(resourcePath)) {
     return;
   }
 
-  const postMethod = resolveObject(oas, path);
+  const postMethod = input;
   if (!postMethod.parameters || postMethod.parameters.length === 0) {
     return;
   }
