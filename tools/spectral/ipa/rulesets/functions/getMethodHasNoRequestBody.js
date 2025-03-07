7ba7b405cf6b98ec89d5b80b92aa5714bca45f8e
Lovisa Berggren <59226031+lovisaberggren@users.noreply.github.com>
2025-03-07 16:57:01 +0000

    CLOUDP-302985: Add rule xgen-IPA-104-get-method-no-request-body (#494)

---
+++ tools/spectral/ipa/rulesets/functions/getMethodHasNoRequestBody.js
@@ -0,0 +1,44 @@
+import { hasException } from './utils/exceptions.js';
+import { collectAdoption, collectAndReturnViolation, collectException } from './utils/collectionUtils.js';
+import {
+  getResourcePathItems,
+  isResourceCollectionIdentifier,
+  isSingleResourceIdentifier,
+  isSingletonResource,
+} from './utils/resourceEvaluation.js';
+
+const RULE_NAME = 'xgen-IPA-104-get-method-no-request-body';
+const ERROR_MESSAGE = 'The Get method must not include a request body.';
+
+export default (input, _, { path, documentInventory }) => {
+  const resourcePath = path[1];
+  const oas = documentInventory.resolved;
+
+  if (
+    !isSingleResourceIdentifier(resourcePath) &&
+    !(
+      isResourceCollectionIdentifier(resourcePath) && isSingletonResource(getResourcePathItems(resourcePath, oas.paths))
+    )
+  ) {
+    return;
+  }
+
+  if (hasException(input, RULE_NAME)) {
+    collectException(input, RULE_NAME, path);
+    return;
+  }
+
+  const errors = checkViolationsAndReturnErrors(input, path);
+
+  if (errors.length !== 0) {
+    return collectAndReturnViolation(path, RULE_NAME, errors);
+  }
+  collectAdoption(path, RULE_NAME);
+};
+
+function checkViolationsAndReturnErrors(getOperationObject, path) {
+  if (getOperationObject.requestBody) {
+    return [{ path, message: ERROR_MESSAGE }];
+  }
+  return [];
+}
