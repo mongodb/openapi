320b534d9e68c901ea90295f7f3c64d8d5dcccd8
Wojciech Trocki <w.trocki@mongodb.com>
2025-03-10 11:40:58 +0100

    CLOUDP-271997: request no body (#489)

--- tools/spectral/ipa/rulesets/functions/deleteMethodNoRequestBody.js
+++ tools/spectral/ipa/rulesets/functions/deleteMethodNoRequestBody.js
@@ -1,11 +1,11 @@
 import { collectAdoption, collectAndReturnViolation, collectException } from './utils/collectionUtils.js';
 import { hasException } from './utils/exceptions.js';
 
-const RULE_NAME = 'xgen-IPA-108-delete-include-404-response';
-const ERROR_MESSAGE = 'DELETE method should include 404 status code for not found resources.';
+const RULE_NAME = 'xgen-IPA-108-delete-request-no-body';
+const ERROR_MESSAGE = 'DELETE method should not have a request body.';
 
 /**
- * Delete method should include 404 status code for not found resources
+ * Delete method should not have a request body
  *
  * @param {object} input - The delete operation object
  * @param {object} _ - Unused
@@ -17,8 +17,8 @@ export default (input, _, { path }) => {
     return;
   }
 
-  const responses = input.responses;
-  if (!responses || !responses['404']) {
+  const requestBody = input.requestBody;
+  if (requestBody) {
     return collectAndReturnViolation(path, RULE_NAME, ERROR_MESSAGE);
   }
   return collectAdoption(path, RULE_NAME);
