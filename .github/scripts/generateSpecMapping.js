c4492f0f289d57478ecf78837aeb8eef35b5a10a
rayangler <27821750+rayangler@users.noreply.github.com>
2025-03-11 15:22:04 -0400

    Fix comments

--- .github/scripts/generateSpecMapping.js
+++ .github/scripts/generateSpecMapping.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Add new specs to be deployed to Bump here
 const SPEC_MAPPING = [
   {
     doc: 'f929d2d7-27d7-4591-b22f-6c2e543a7874',
@@ -13,9 +14,6 @@ const SPEC_MAPPING = [
   },
 ];
 
-/**
- * Handles the resource versions for Atlas Admin API v2
- */
 function handleAdminAPIv2() {
   const docId = '2accf4b8-a543-426c-94c3-794ae26b68be';
   const directory = 'openapi/v2';
