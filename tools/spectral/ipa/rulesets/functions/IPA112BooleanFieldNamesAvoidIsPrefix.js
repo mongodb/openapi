647dab7764b75122db16f39c5ddd96b69863f057
Yeliz Henden <yeliz.henden@mongodb.com>
2025-03-24 10:35:11 +0000

    address the comments

--- tools/spectral/ipa/rulesets/functions/IPA112BooleanFieldNamesAvoidIsPrefix.js
+++ tools/spectral/ipa/rulesets/functions/IPA112BooleanFieldNamesAvoidIsPrefix.js
@@ -14,12 +14,14 @@ export default (input, options, { path, documentInventory }) => {
     return;
   }
 
-  if (property.type === 'boolean') {
-    if (IS_PREFIX_REGEX.test(input)) {
-      const suggestedName = input.charAt(2).toLowerCase() + input.slice(3);
-      const errorMessage = `Boolean field "${input}" should not use the "is" prefix. Use "${suggestedName}" instead.`;
-      return collectAndReturnViolation(path, RULE_NAME, errorMessage);
-    }
+  if (property.type !== 'boolean') {
+    return;
+  }
+
+  if (IS_PREFIX_REGEX.test(input)) {
+    const suggestedName = input.charAt(2).toLowerCase() + input.slice(3);
+    const errorMessage = `Boolean field "${input}" should not use the "is" prefix. Use "${suggestedName}" instead.`;
+    return collectAndReturnViolation(path, RULE_NAME, errorMessage);
   }
 
   collectAdoption(path, RULE_NAME);
