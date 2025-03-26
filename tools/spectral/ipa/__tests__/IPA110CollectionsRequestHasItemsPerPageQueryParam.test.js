c5963607badbf569c1210acf8a1c0ee74396e406
Yeliz Henden <165907936+yelizhenden-mdb@users.noreply.github.com>
2025-03-26 15:12:21 +0000

    IPA-110: Pagination (itemsPerPage and pageNum query params) (#617)

---
+++ tools/spectral/ipa/__tests__/IPA110CollectionsRequestHasItemsPerPageQueryParam.test.js
@@ -0,0 +1,253 @@
+import testRule from './__helpers__/testRule';
+import { DiagnosticSeverity } from '@stoplight/types';
+
+const parameters = {
+  parameters: {
+    pageNum: {
+      name: 'pageNum',
+      in: 'query',
+      schema: {
+        type: 'integer',
+        default: 1,
+      },
+    },
+    itemsPerPage: {
+      name: 'itemsPerPage',
+      in: 'query',
+      schema: {
+        type: 'integer',
+        default: 100,
+      },
+    },
+  },
+};
+
+testRule('xgen-IPA-110-collections-request-has-itemsPerPage-query-param', [
+  {
+    name: 'valid examples',
+    document: {
+      paths: {
+        '/resources': {
+          get: {
+            parameters: [
+              {
+                name: 'itemsPerPage',
+                in: 'query',
+                schema: {
+                  type: 'integer',
+                  default: 100,
+                },
+              },
+            ],
+          },
+        },
+        'resources/{resourceId}': {
+          get: {},
+        },
+        '/resourcesTwo': {
+          get: {
+            parameters: [
+              {
+                $ref: '#/components/parameters/itemsPerPage',
+              },
+            ],
+          },
+        },
+        'resourcesTwo/{resourceId}': {
+          get: {},
+        },
+      },
+      components: parameters,
+    },
+    errors: [],
+  },
+  {
+    name: 'invalid - missing parameters',
+    document: {
+      paths: {
+        '/resources': {
+          get: {},
+        },
+      },
+    },
+    errors: [
+      {
+        code: 'xgen-IPA-110-collections-request-has-itemsPerPage-query-param',
+        message: 'List method is missing query parameters.',
+        path: ['paths', '/resources', 'get'],
+        severity: DiagnosticSeverity.Warning,
+      },
+    ],
+  },
+  {
+    name: 'invalid - missing itemsPerPage parameter',
+    document: {
+      paths: {
+        '/resources': {
+          get: {
+            parameters: [
+              {
+                name: 'pageNum',
+                in: 'query',
+                schema: {
+                  type: 'integer',
+                  default: 1,
+                },
+              },
+            ],
+          },
+        },
+        'resources/{resourceId}': {
+          get: {},
+        },
+        '/resourcesTwo': {
+          get: {
+            parameters: [
+              {
+                $ref: '#/components/parameters/pageNum',
+              },
+            ],
+          },
+        },
+        'resourcesTwo/{resourceId}': {
+          get: {},
+        },
+      },
+      components: parameters,
+    },
+    errors: [
+      {
+        code: 'xgen-IPA-110-collections-request-has-itemsPerPage-query-param',
+        message: 'List method is missing a itemsPerPage query parameter.',
+        path: ['paths', '/resources', 'get'],
+        severity: DiagnosticSeverity.Warning,
+      },
+      {
+        code: 'xgen-IPA-110-collections-request-has-itemsPerPage-query-param',
+        message: 'List method is missing a itemsPerPage query parameter.',
+        path: ['paths', '/resourcesTwo', 'get'],
+        severity: DiagnosticSeverity.Warning,
+      },
+    ],
+  },
+  {
+    name: 'invalid - itemsPerPage parameter is required',
+    document: {
+      paths: {
+        '/resources': {
+          get: {
+            parameters: [
+              {
+                name: 'itemsPerPage',
+                in: 'query',
+                required: true,
+                schema: {
+                  type: 'integer',
+                  default: 100,
+                },
+              },
+            ],
+          },
+        },
+        'resources/{resourceId}': {
+          get: {},
+        },
+      },
+    },
+    errors: [
+      {
+        code: 'xgen-IPA-110-collections-request-has-itemsPerPage-query-param',
+        message: 'itemsPerPage query parameter of List method must not be required.',
+        path: ['paths', '/resources', 'get'],
+        severity: DiagnosticSeverity.Warning,
+      },
+    ],
+  },
+  {
+    name: 'invalid - itemsPerPage parameter without default value',
+    document: {
+      paths: {
+        '/resources': {
+          get: {
+            parameters: [
+              {
+                name: 'itemsPerPage',
+                in: 'query',
+                schema: {
+                  type: 'integer',
+                },
+              },
+            ],
+          },
+        },
+        'resources/{resourceId}': {
+          get: {},
+        },
+      },
+    },
+    errors: [
+      {
+        code: 'xgen-IPA-110-collections-request-has-itemsPerPage-query-param',
+        message: 'itemsPerPage query parameter of List method must have a default value defined.',
+        path: ['paths', '/resources', 'get'],
+        severity: DiagnosticSeverity.Warning,
+      },
+    ],
+  },
+  {
+    name: 'invalid - wrong default value',
+    document: {
+      paths: {
+        '/resources': {
+          get: {
+            parameters: [
+              {
+                name: 'itemsPerPage',
+                in: 'query',
+                schema: {
+                  type: 'integer',
+                  default: 0,
+                },
+              },
+            ],
+          },
+        },
+        'resources/{resourceId}': {
+          get: {},
+        },
+      },
+    },
+    errors: [
+      {
+        code: 'xgen-IPA-110-collections-request-has-itemsPerPage-query-param',
+        message: 'itemsPerPage query parameter of List method must have a default value of 100.',
+        path: ['paths', '/resources', 'get'],
+        severity: DiagnosticSeverity.Warning,
+      },
+    ],
+  },
+  {
+    name: 'exception',
+    document: {
+      paths: {
+        '/resourcesTwo': {
+          get: {
+            parameters: [
+              {
+                $ref: '#/components/parameters/pageNum',
+              },
+            ],
+            'x-xgen-IPA-exception': {
+              'xgen-IPA-110-collections-request-has-itemsPerPage-query-param': 'Reason',
+            },
+          },
+        },
+        'resourcesTwo/{resourceId}': {
+          get: {},
+        },
+      },
+      components: parameters,
+    },
+    errors: [],
+  },
+]);
