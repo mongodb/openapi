8b2ef26b33c340c899b30f2db4e99fcaabecab16
Yeliz Henden <yeliz.henden@mongodb.com>
2025-04-02 15:35:30 +0100

    address the comments

--- tools/spectral/ipa/rulesets/functions/IPA121DateTimeFieldsMentionISO8601.js
+++ tools/spectral/ipa/rulesets/functions/IPA121DateTimeFieldsMentionISO8601.js
@@ -1,19 +1,12 @@
 import { hasException } from './utils/exceptions.js';
 import { collectAdoption, collectAndReturnViolation, collectException } from './utils/collectionUtils.js';
-import { resolveObject } from './utils/componentUtils.js';
 
 const RULE_NAME = 'xgen-IPA-121-date-time-fields-mention-iso-8601';
 const ERROR_MESSAGE =
   'API producers must use ISO 8601 date-time format in UTC for all timestamps. Fields must note ISO 8601 and UTC in their description.';
 
-export default (input, options, { path, documentInventory }) => {
-  const oas = documentInventory.unresolved;
-  const propertyObject = resolveObject(oas, path);
+export default (input, options, { path }) => {
   const fieldType = path[path.length - 2];
-  // Not to duplicate the check for referenced schemas
-  if (!propertyObject) {
-    return;
-  }
 
   if (hasException(input, RULE_NAME)) {
     collectException(input, RULE_NAME, path);
