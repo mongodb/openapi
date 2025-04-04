fc990a0f8b3ccb7d5fa9665f28aa38c27dec45ed
Yeliz Henden <yeliz.henden@mongodb.com>
2025-04-04 16:16:25 +0100

    address the comments

--- tools/spectral/ipa/rulesets/functions/IPA126TagNamesShouldUseTitleCase.js
+++ tools/spectral/ipa/rulesets/functions/IPA126TagNamesShouldUseTitleCase.js
@@ -6,10 +6,6 @@ const RULE_NAME = 'xgen-IPA-126-tag-names-should-use-title-case';
 
 export default (input, options, { path }) => {
   const tagName = input.name;
-  if (!tagName || tagName.trim().length === 0) {
-    return;
-  }
-
   if (hasException(input, RULE_NAME)) {
     collectException(input, RULE_NAME, path);
     return;
