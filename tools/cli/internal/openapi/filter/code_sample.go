fd69e123b967775a5e26d545dc9d65be5b41a694
andreaangiolillo <andrea.angiolillo@mongodb.com>
2025-05-20 14:05:07 +0200

    added sample

---
+++ tools/cli/internal/openapi/filter/code_sample.go
@@ -0,0 +1,91 @@
+// Copyright 2025 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package filter
+
+import (
+	"time"
+
+	"github.com/getkin/kin-openapi/openapi3"
+)
+
+const codeSampleExtensionName = "x-codeSamples"
+
+// https://redocly.com/docs-legacy/api-reference-docs/specification-extensions/x-code-samples#x-codesamples
+type codeSample struct {
+	Lang   string `json:"lang,omitempty" yaml:"lang,omitempty"`
+	Label  string `json:"label,omitempty" yaml:"label,omitempty"`
+	Source string `json:"source,omitempty" yaml:"source,omitempty"`
+}
+
+// CodeSampleFilter modifies includes the fields "x-state" and "x-beta" to the "preview" and "upcoming" APIs Operations.
+// The "x-state" and "x-beta" fields are bump.sh custom fields to include budges
+// Bump.sh feature: https://docs.bump.sh/help/specification-support/doc-code-samples/#example-usage
+type CodeSampleFilter struct {
+	oas      *openapi3.T
+	metadata *Metadata
+}
+
+func (f *CodeSampleFilter) ValidateMetadata() error {
+	return validateMetadataWithVersion(f.metadata)
+}
+
+func (f *CodeSampleFilter) Apply() error {
+	for pathName, p := range f.oas.Paths.Map() {
+		for opK, op := range p.Operations() {
+			if err := f.includeCodeSamplesForOperation(pathName, opK, op); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (f *CodeSampleFilter) newCurlCodeSamplesForOperation(pathName, opK string, op *openapi3.Operation) codeSample {
+	source := "curl --user \"{PUBLIC-KEY}:{PRIVATE-KEY}\" \\\n  --digest \\\n  " +
+		"--header \"Accept: application/vnd.atlas." + f.metadata.targetVersion.Date().Format(time.DateOnly) + "+json\" \\\n  "
+
+	switch opK {
+	case "GET":
+		source += "-X " + opK + " \"" + pathName + "?pretty=true\""
+	case "DELETE":
+		source += "-X " + opK + " \"" + pathName + "\""
+	case "POST", "PATCH", "PUT":
+		source += "-X " + opK + " \"" + pathName + "\"\n  "
+		source += "-d " + "{ <Payload> }"
+	}
+
+	return codeSample{
+		Lang:   "cURL",
+		Label:  "curl",
+		Source: source,
+	}
+}
+
+func (f *CodeSampleFilter) newAtlasCliCodeSamplesForOperation(op *openapi3.Operation) codeSample {
+	return codeSample{
+		Lang:   "cURL",
+		Label:  "Atlas CLI",
+		Source: "atlas api " + op.OperationID + " --help",
+	}
+}
+
+func (f *CodeSampleFilter) includeCodeSamplesForOperation(pathName, opK string, op *openapi3.Operation) error {
+	op.Extensions[codeSampleExtensionName] = []codeSample{
+		f.newCurlCodeSamplesForOperation(pathName, opK, op),
+		f.newAtlasCliCodeSamplesForOperation(op),
+	}
+	return nil
+}
