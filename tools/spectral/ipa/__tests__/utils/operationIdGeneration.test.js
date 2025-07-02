b4926745b2fe93f103fc32612e3d75953937b9f8
Sophia Marie Terry <sophia.terry@mongodb.com>
2025-07-02 11:36:51 +0100

    CLOUDP-328283: Added tests for Operation ID generation

---
+++ tools/spectral/ipa/__tests__/utils/operationIdGeneration.test.js
@@ -0,0 +1,29 @@
+import { describe, expect, it } from '@jest/globals';
+import { generateOperationID } from '../../rulesets/functions/utils/operationIdGeneration';
+
+describe('tools/spectral/ipa/utils/operationIdGeneration.js', () => {
+    it('should singularize all nouns', () => {
+     expect(generateOperationID("create", "/groups/{groupId}/clusters")).toEqual("createGroupCluster");
+     expect(generateOperationID("delete", "/groups/{groupId}/clusters/{clusterName}")).toEqual("deleteGroupCluster");
+     expect(generateOperationID("get", "/groups/{groupId}/clusters/{clusterName}")).toEqual("getGroupCluster");
+     expect(generateOperationID("update", "/groups/{groupId}/clusters/{clusterName}")).toEqual("updateGroupCluster");
+     expect(generateOperationID("pause", "/groups/{groupId}/clusters/{clusterName}")).toEqual("pauseGroupCluster");
+    })
+
+    it('should leave the final noun as is', () => {
+     expect(generateOperationID("list", "/groups/{groupId}/clusters")).toEqual("listGroupClusters");
+     expect(generateOperationID("get", "/groups/{groupId}/settings")).toEqual("getGroupSettings");
+     expect(generateOperationID("update", "/groups/{groupId}/settings")).toEqual("updateGroupSettings");
+     expect(generateOperationID("search", "/groups/{groupId}/clusters")).toEqual("searchGroupClusters");
+     expect(generateOperationID("get", "/groups/{groupId}/clusters/{clusterName}/{clusterView}/{databaseName}/{collectionName}/collStats/measurements")).toEqual("getGroupClusterCollStatMeasurements");
+     expect(generateOperationID("grant", "/api/atlas/v2/groups/{groupId}/access")).toEqual("grantGroupAccess");
+    })
+
+    it('should split camelCase method names', () => {
+     expect(generateOperationID("addNode", "/groups/{groupId}/clusters/{clusterName}")).toEqual("addGroupClusterNode");
+    })
+
+    it('should accomodate legacy custom methods', () => {
+     expect(generateOperationID("", "/api/atlas/v2/groups/{groupId}/clusters/{clusterName}/restartPrimaries")).toEqual("restartGroupClusterPrimaries");
+    })
+})
