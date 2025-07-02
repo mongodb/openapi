7852f59f594ca830d8821eba80f93199ea4d98bb
Sophia Marie Terry <sophia.terry@mongodb.com>
2025-07-02 15:09:01 +0100

    CLOUDP-328283: Prettier

--- tools/spectral/ipa/rulesets/functions/utils/operationIdGeneration.js
+++ tools/spectral/ipa/rulesets/functions/utils/operationIdGeneration.js
@@ -1,62 +1,62 @@
-import { singularize } from "ember-inflector";
+import { singularize } from 'ember-inflector';
 import { isPathParam, removePrefix, isSingleResourceIdentifier } from './resourceEvaluation.js';
 import { casing } from '@stoplight/spectral-functions';
 
 /**
  * Returns IPA Compliant Operation ID.
- * 
+ *
  * @param method the standard method name (create, update, get etc.), custom method name, or empty string (only for legacy custom methods)
  * @param path the path for the endpoint
  */
 export function generateOperationID(method, path) {
-    let resourceIdentifier = removePrefix(path);
-    let nouns = resourceIdentifier
-                    .split("/")
-                    .filter(id => id.length > 0 && !isPathParam(id))
-                    .map(noun => capitalize(noun));
-    
-    // legacy custom method - use end of path as custom method name
-    if (method === "") {
-        method = path.split("/").pop();
-        nouns.pop();
-    }
+  let resourceIdentifier = removePrefix(path);
+  let nouns = resourceIdentifier
+    .split('/')
+    .filter((id) => id.length > 0 && !isPathParam(id))
+    .map((noun) => capitalize(noun));
 
-    let verb = deriveActionVerb(method);
-    
-    // if custom method name is multiple words, add trailing nouns to the operation ID
-    if (!casing(method, { type: 'camel'}) && method.length > verb.length) {
-        nouns.push(method.slice(verb.length));
-    }
+  // legacy custom method - use end of path as custom method name
+  if (method === '') {
+    method = path.split('/').pop();
+    nouns.pop();
+  }
 
-    let opID = verb;
-    for(let i = 0; i < nouns.length - 1; i++) {
-        opID += singularize(nouns[i]);
-    }
-    
-    // singularize final noun, dependent on resource identifier
-    if (isSingleResourceIdentifier(resourceIdentifier) || verb == "create") {
-        nouns[nouns.length - 1] = singularize(nouns[nouns.length - 1]);
-    } 
+  let verb = deriveActionVerb(method);
 
-    opID += nouns.pop();
+  // if custom method name is multiple words, add trailing nouns to the operation ID
+  if (!casing(method, { type: 'camel' }) && method.length > verb.length) {
+    nouns.push(method.slice(verb.length));
+  }
 
-    return opID;
+  let opID = verb;
+  for (let i = 0; i < nouns.length - 1; i++) {
+    opID += singularize(nouns[i]);
+  }
+
+  // singularize final noun, dependent on resource identifier
+  if (isSingleResourceIdentifier(resourceIdentifier) || verb == 'create') {
+    nouns[nouns.length - 1] = singularize(nouns[nouns.length - 1]);
+  }
+
+  opID += nouns.pop();
+
+  return opID;
 }
 
 /**
  * Derives action verb from custom method name. Returns standard method names as is.
- * 
+ *
  * @param method the custom method name
  */
 function deriveActionVerb(method) {
-    // custom method name is camelCase return first word (assumed verb)
-    if (!casing(method, { type: 'camel'})) {
-        return method.match(/[A-Z]?[a-z]+/g)[0];
-    }
+  // custom method name is camelCase return first word (assumed verb)
+  if (!casing(method, { type: 'camel' })) {
+    return method.match(/[A-Z]?[a-z]+/g)[0];
+  }
 
-    return method;
+  return method;
 }
 
 function capitalize(val) {
-    return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+  return String(val).charAt(0).toUpperCase() + String(val).slice(1);
 }
