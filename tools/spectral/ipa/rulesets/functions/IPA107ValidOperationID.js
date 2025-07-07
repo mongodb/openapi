769a4a08a71218ab2fd4a328658a717e3abe5be6
Sophia Marie Terry <sophia.terry@mongodb.com>
2025-07-07 18:05:41 +0100

    CLOUDP-328959: Added opID validation yaml for IPA107 and fixed lint error

--- tools/spectral/ipa/rulesets/functions/IPA107ValidOperationID.js
+++ tools/spectral/ipa/rulesets/functions/IPA107ValidOperationID.js
@@ -7,7 +7,7 @@ const RULE_NAME = 'xgen-IPA-107-valid-operation-id';
 const ERROR_MESSAGE =
   'Invalid OperationID. The Operation ID must start with the verb “update” and should be followed by a noun or compound noun. The noun(s) in the Operation ID should be the collection identifiers from the resource identifier in singular form. For singleton resources - the last noun may be in plural form.';
 
-export default (input, _, { path, _ }) => {
+export default (input, _, { path }) => {
   let resourcePath = path[1];
   let methodName = 'update';
 
