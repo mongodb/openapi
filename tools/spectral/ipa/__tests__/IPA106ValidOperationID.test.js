ff7df80fc7cb5cfc5528ee156ed1f36d2e221126
Sophia Marie Terry <sophia.terry@mongodb.com>
2025-07-08 15:16:42 +0100

    CLOUDP-328959: Added tests for validating IPA106 valid operationIds

---
+++ tools/spectral/ipa/__tests__/IPA106ValidOperationID.test.js
@@ -0,0 +1,82 @@
+import testRule from './__helpers__/testRule';
+
+// TODO: add tests for xgen-custom-method extension - CLOUDP-306294
+// TOOD: enable tests for invalid methods (after rules are upgraded to warning) - CLOUDP-329722
+
+const componentSchemas = {
+  schemas: {
+    Schema: {
+      type: 'object',
+    },
+  },
+  operationId: 'string',
+};
+
+testRule('xgen-IPA-106-valid-operation-id', [
+  {
+    name: 'valid methods',
+    document: {
+      components: componentSchemas,
+      paths: {
+        '/groups/{groupId}/clusters': {
+          post: {
+            operationId: 'createGroupCluster',
+          },
+        },
+      },
+    },
+    errors: [],
+  },
+  // This test will be enable when the xgen-IPA-106-valid-operation-id is set to warning severity - CLOUDP-329722
+  /* {
+    name: 'invalid methods',
+    document: {
+      components: componentSchemas,
+      paths: {
+        '/api/atlas/v2/groups/{groupId}/access': {
+          post: {
+            operationId: 'addUserToProject',
+          },
+        },
+        '/api/atlas/v2/groups/{groupId}/invites': {
+          post: {
+            operationId: 'createProjectInvitation',
+          },
+        },
+      },
+    },
+    errors: [
+      {
+        code: 'xgen-IPA-106-valid-operation-id',
+        message:
+          'Invalid OperationID. The Operation ID must start with the verb “create” and should be followed by a noun or compound noun. The noun(s) in the Operation ID should be the collection identifiers from the resource identifier in singular form. ',
+        path: ['paths', '/api/atlas/v2/groups/{groupId}/access', 'post'],
+        severity: DiagnosticSeverity.Warning,
+      },
+      {
+        code: 'xgen-IPA-106-valid-operation-id',
+        message:
+          'Invalid OperationID. The Operation ID must start with the verb “create” and should be followed by a noun or compound noun. The noun(s) in the Operation ID should be the collection identifiers from the resource identifier in singular form. ',
+        path: ['paths', '/api/atlas/v2/groups/{groupId}/invites', 'post'],
+        severity: DiagnosticSeverity.Warning,
+      },
+    ],
+  }, */
+  {
+    name: 'invalid methods with exceptions',
+    document: {
+      components: componentSchemas,
+      paths: {
+        '/api/atlas/v2/groups/{groupId}/clusters/{clusterName}/index ': {
+          post: {
+            operationId: 'createRollingIndex',
+            'x-xgen-IPA-exception': {
+              'xgen-IPA-106-valid-operation-id': 'Reason',
+            },
+          },
+        },
+      },
+    },
+    errors: [],
+  },
+]);
