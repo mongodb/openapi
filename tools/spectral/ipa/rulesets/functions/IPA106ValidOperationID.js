d3b721ae391f32ade6bfb11941679518917eab87
Sophia Terry <157913563+sphterry@users.noreply.github.com>
2025-07-09 13:26:23 +0100

    CLOUDP 328959: OperationID Validation functions for Create, Delete, and Update methods (#805)

---
+++ tools/spectral/ipa/rulesets/functions/IPA106ValidOperationID.js
@@ -0,0 +1,42 @@
+import { hasException } from './utils/exceptions.js';
+import { collectAdoption, collectException, collectAndReturnViolation } from './utils/collectionUtils.js';
+import {
+  isCustomMethodIdentifier,
+  isResourceCollectionIdentifier,
+  isSingletonResource,
+  getResourcePathItems,
+} from './utils/resourceEvaluation.js';
+import { generateOperationID } from './utils/operationIdGeneration.js';
+
+const RULE_NAME = 'xgen-IPA-106-valid-operation-id';
+const ERROR_MESSAGE =
+  'Invalid OperationID. The Operation ID must start with the verb “create” and should be followed by a noun or compound noun. The noun(s) in the Operation ID should be the collection identifiers from the resource identifier in singular form.';
+
+export default (input, { methodName }, { path, documentInventory }) => {
+  const resourcePath = path[1];
+  const oas = documentInventory.resolved;
+  const resourcePaths = getResourcePathItems(resourcePath, oas.paths);
+
+  const isResourceCollection = isResourceCollectionIdentifier(resourcePath) && !isSingletonResource(resourcePaths);
+  if (isCustomMethodIdentifier(resourcePath) || !isResourceCollection) {
+    return;
+  }
+
+  if (hasException(input, RULE_NAME)) {
+    collectException(input, RULE_NAME, path);
+    return;
+  }
+
+  const expectedOperationID = generateOperationID(methodName, resourcePath);
+  if (expectedOperationID !== input.operationId) {
+    const errors = [
+      {
+        path,
+        message: `${ERROR_MESSAGE} Found ${input.operationId}, expected ${expectedOperationID}.`,
+      },
+    ];
+    return collectAndReturnViolation(path, RULE_NAME, errors);
+  }
+
+  collectAdoption(path, RULE_NAME);
+};
