659e9cafe7231b3f918ad7316a18d862633e0b90
Sophia Terry <157913563+sphterry@users.noreply.github.com>
2025-07-09 17:26:53 +0100

    CLOUDP 328955: OperationID Validation functions for Get and List methods (#814)

---
+++ tools/spectral/ipa/__tests__/IPA104ValidOperationID.test.js
@@ -0,0 +1,70 @@
+import testRule from './__helpers__/testRule';
+
+// TODO: add tests for xgen-custom-method extension - CLOUDP-306294
+// TOOD: enable tests for invalid methods (after rules are upgraded to warning) - CLOUDP-329722
+
+testRule('xgen-IPA-104-valid-operation-id', [
+  {
+    name: 'valid methods',
+    document: {
+      paths: {
+        '/groups/{groupId}/cluster/{clusterName}': {
+          get: {
+            operationId: 'getGroupCluster',
+          },
+        },
+      },
+    },
+    errors: [],
+  },
+  // This test will be enable when the xgen-IPA-104-valid-operation-id is set to warning severity - CLOUDP-329722
+  /* {
+    name: 'invalid methods',
+    document: {
+      paths: {
+        '/api/atlas/v2/groups/{groupId}/accessList/{entryValue}/status': {
+          get: {
+            operationId: 'getProjectIpAccessListStatus',
+          },
+        },
+        '/api/atlas/v2/groups/{groupId}/dataFederation/{tenantName}/limits/{limitName}': {
+          get: {
+            operationId: 'returnFederatedDatabaseQueryLimit',
+          },
+        },
+      },
+    },
+    errors: [
+      {
+        code: 'xgen-IPA-104-valid-operation-id',
+        message:
+          'Invalid OperationID. The Operation ID must start with the verb “get” and should be followed by a noun or compound noun. The noun(s) should be the collection identifiers from the resource identifier in singular form.',
+        path: ['paths', '/api/atlas/v2/groups/{groupId}/accessList/{entryValue}/status', 'get'],
+        severity: DiagnosticSeverity.Warning,
+      },
+      {
+        code: 'xgen-IPA-104-valid-operation-id',
+        message:
+          'Invalid OperationID. The Operation ID must start with the verb “get” and should be followed by a noun or compound noun. The noun(s) should be the collection identifiers from the resource identifier in singular form.',
+        path: ['paths', '/api/atlas/v2/groups/{groupId}/dataFederation/{tenantName}/limits/{limitName}', 'get'],
+        severity: DiagnosticSeverity.Warning,
+      },
+    ],
+  }, */
+  {
+    name: 'invalid methods with exceptions',
+    document: {
+      paths: {
+        '/api/atlas/v2/groups/{groupId}/clusters/{clusterName}/index ': {
+          get: {
+            operationId: 'getRollingIndex',
+            'x-xgen-IPA-exception': {
+              'xgen-IPA-104-valid-operation-id': 'Reason',
+            },
+          },
+        },
+      },
+    },
+    errors: [],
+  },
+]);
--- tools/spectral/ipa/rulesets/functions/IPA105ValidOperationID.js
+++ tools/spectral/ipa/rulesets/functions/IPA105ValidOperationID.js
@@ -1,23 +1,25 @@
 import { hasException } from './utils/exceptions.js';
-import { collectAdoption, collectException, collectAndReturnViolation } from './utils/collectionUtils.js';
+import { collectAdoption, collectAndReturnViolation, collectException } from './utils/collectionUtils.js';
 import {
+  getResourcePathItems,
   isCustomMethodIdentifier,
   isResourceCollectionIdentifier,
   isSingletonResource,
-  getResourcePathItems,
 } from './utils/resourceEvaluation.js';
 import { generateOperationID } from './utils/operationIdGeneration.js';
 
-const RULE_NAME = 'xgen-IPA-106-valid-operation-id';
+const RULE_NAME = 'xgen-IPA-105-valid-operation-id';
 const ERROR_MESSAGE = 'Invalid OperationID.';
 
 export default (input, { methodName }, { path, documentInventory }) => {
   const resourcePath = path[1];
   const oas = documentInventory.resolved;
-  const resourcePaths = getResourcePathItems(resourcePath, oas.paths);
 
-  const isResourceCollection = isResourceCollectionIdentifier(resourcePath) && !isSingletonResource(resourcePaths);
-  if (isCustomMethodIdentifier(resourcePath) || !isResourceCollection) {
+  if (
+    isCustomMethodIdentifier(resourcePath) ||
+    !isResourceCollectionIdentifier(resourcePath) ||
+    isSingletonResource(getResourcePathItems(resourcePath, oas.paths))
+  ) {
     return;
   }
 
@@ -26,16 +28,16 @@ export default (input, { methodName }, { path, documentInventory }) => {
     return;
   }
 
-  const expectedOperationID = generateOperationID(methodName, resourcePath);
-  if (expectedOperationID !== input.operationId) {
+  const expectedOperationId = generateOperationID(methodName, resourcePath);
+  if (expectedOperationId !== input.operationId) {
     const errors = [
       {
         path,
-        message: `${ERROR_MESSAGE} Found ${input.operationId}, expected ${expectedOperationID}.`,
+        message: `${ERROR_MESSAGE} Found ${input.operationId}, expected ${expectedOperationId}.`,
       },
     ];
     return collectAndReturnViolation(path, RULE_NAME, errors);
   }
 
-  collectAdoption(path, RULE_NAME);
+  return collectAdoption(path, RULE_NAME);
 };
