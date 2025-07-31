f4f3a0919f6e598bfbadb0d0040628f42acec7eb
Sophia Marie Terry <sophia.terry@mongodb.com>
2025-07-31 13:30:33 +0100

    remove unbound variable

--- .github/scripts/ipa_version_check.sh
+++ .github/scripts/ipa_version_check.sh
@@ -3,7 +3,7 @@ set -eou pipefail
   
 current_version=$(jq -r '.version' tools/spectral/ipa/package.json)  
 
-previous_version=$(git show origin/"${BASE_BRANCH}":tools/spectral/ipa/package.json | jq -r '.version')  
+previous_version=$(git show origin/main:tools/spectral/ipa/package.json | jq -r '.version')  
   
 if [[ -z "$previous_version" || "$previous_version" == "null" ]]; then  
   previous_version="none"  
