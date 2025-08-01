be25cb4d2762a5258c557083daf5037091020df0
Sophia Marie Terry <sophia.terry@mongodb.com>
2025-08-01 15:28:11 +0100

    Point to previous commit

--- .github/scripts/ipa_version_check.sh
+++ .github/scripts/ipa_version_check.sh
@@ -3,7 +3,7 @@ set -eou pipefail
     
 current_version=$(jq -r '.version' tools/spectral/ipa/package.json)    
   
-previous_version=$(git show HEAD^:tools/spectral/ipa/package.json | jq -r '.version' || echo "none")    
+previous_version=$(git show HEAD~1:tools/spectral/ipa/package.json | jq -r '.version' || echo "none")    
   
 if [[ "${previous_version}" == "${current_version}" ]]; then    
   echo "false"    
