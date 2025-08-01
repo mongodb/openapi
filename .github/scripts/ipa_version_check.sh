75909296ae4b5d96e88dce79bd8c5e305385487a
Sophia Terry <157913563+sphterry@users.noreply.github.com>
2025-08-01 11:27:03 +0100

    feat(ipa): IPA Package config, docs and release workflow (#855)

---
+++ .github/scripts/ipa_version_check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash  
+set -eou pipefail
+  
+current_version=$(jq -r '.version' tools/spectral/ipa/package.json)  
+
+previous_version=$(git show origin/"${BASE_BRANCH}":tools/spectral/ipa/package.json | jq -r '.version')  
+  
+if [[ -z "${previous_version}" || "${previous_version}" == "null" ]]; then  
+  previous_version="none"  
+fi  
+  
+if [[ "${previous_version}" == "${current_version}" ]]; then  
+  echo "false"  
+else  
+  echo "true"  
+fi  
