900c47b92c03a1280785b7a229aca443b49711e7
Yeliz Henden <165907936+yelizhenden-mdb@users.noreply.github.com>
2025-08-13 15:19:31 +0100

    fix(ipa): Validate both inline and reusable enums (#890)

--- tools/spectral/ipa/rulesets/functions/IPA123EnumValuesShouldNotExceed20.js
+++ tools/spectral/ipa/rulesets/functions/IPA123EnumValuesShouldNotExceed20.js
@@ -3,17 +3,12 @@ import { getSchemaPathFromEnumPath } from './utils/schemaUtils.js';
 import { resolveObject } from './utils/componentUtils.js';
 
 const RULE_NAME = 'xgen-IPA-123-allowable-enum-values-should-not-exceed-20';
-const ERROR_MESSAGE = 'Inline enum arrays should not exceed 20 values. Current count: ';
+const ERROR_MESSAGE = 'The number of allowable enum values should not exceed 20 values. Current count: ';
 
 export default (input, { maxEnumValues }, { path, documentInventory }) => {
   const oas = documentInventory.resolved;
   const schemaPath = getSchemaPathFromEnumPath(path);
   const schemaObject = resolveObject(oas, schemaPath);
-
-  //Ignore if the schemaObject belongs to a reusable enum
-  if (!schemaPath.includes('properties') && !schemaPath.includes('parameters')) {
-    return;
-  }
 
   if (!Array.isArray(input)) {
     return;
