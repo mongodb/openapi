4397cf06bc7051cf8ca6f4e1aa139e66e7f5d831
Lovisa Berggren <59226031+lovisaberggren@users.noreply.github.com>
2025-08-15 17:32:03 +0100

    feat(ipa): new rule xgen-IPA-117-operation-summary-format (#897)

---
+++ tools/spectral/ipa/rulesets/functions/IPA117OperationSummaryFormat.js
@@ -0,0 +1,26 @@
+import { evaluateAndCollectAdoptionStatus, handleInternalError } from './utils/collectionUtils.js';
+import { isTitleCase } from './utils/casing.js';
+import { resolveObject } from './utils/componentUtils.js';
+
+export default (input, { ignoreList, grammaticalWords }, { path, rule, documentInventory }) => {
+  const operationObjectPath = path.slice(0, -1);
+  const operationObject = resolveObject(documentInventory.resolved, operationObjectPath);
+  const errors = checkViolationsAndReturnErrors(input, ignoreList, grammaticalWords, operationObjectPath, rule.name);
+  return evaluateAndCollectAdoptionStatus(errors, rule.name, operationObject, operationObjectPath);
+};
+
+function checkViolationsAndReturnErrors(summary, ignoreList, grammaticalWords, path, ruleName) {
+  try {
+    if (!isTitleCase(summary, ignoreList, grammaticalWords)) {
+      return [
+        {
+          path,
+          message: `Operation summaries must be in Title Case, must not end with a period and must not use CommonMark.`,
+        },
+      ];
+    }
+    return [];
+  } catch (e) {
+    handleInternalError(ruleName, path, e);
+  }
+}
