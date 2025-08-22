f931b92e9bee95d7dd71de4f480c176ffde51f69
Lovisa Berggren <lovisa.berggren@mongodb.com>
2025-08-22 17:14:14 +0100

    POC: spellcheck

---
+++ tools/spectral/ipa/rulesets/functions/IPA100Spellcheck.js
@@ -0,0 +1,29 @@
+import { evaluateAndCollectAdoptionStatus, handleInternalError } from './utils/collectionUtils.js';
+
+import spellcheck from 'markdown-spellcheck';
+
+export default (input, { customWords, spellcheckOptions }, { path, rule }) => {
+  const errors = checkViolationsAndReturnErrors(input, customWords, spellcheckOptions, path, rule.name);
+  return evaluateAndCollectAdoptionStatus(errors, rule.name, input, path);
+};
+
+function checkViolationsAndReturnErrors(text, customWords, spellcheckOptions, path, ruleName) {
+  try {
+    const errors = [];
+    const results = spellcheck.spell(text, spellcheckOptions);
+    results.forEach((error) => {
+      if (!customWords.includes(error.word)) {
+        errors.push({
+          path,
+          message: `Spelling error: "${error.word}" at position ${error.index}`,
+        });
+      }
+    });
+    if (errors.length !== 0) {
+      console.log('input\t', text, '\n');
+    }
+    return errors;
+  } catch (e) {
+    return handleInternalError(ruleName, path, e);
+  }
+}
